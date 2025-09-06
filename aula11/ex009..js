8268eb6f503b90408cda8986e8e9f46ef9694185
Erick Silva <donizttierick@gmail.com>
2025-09-06 16:45:50 -0300

    .

--- aula11/ex009..js
+++ aula11/ex009..js
@@ -1,1 +1,6 @@
-console.log('Funcionou normal ')
+var vel = 100
+
+if (vel > 70){
+    console.log(`Voce ultrapassou a velocidade de ${vel} km/h. Multado!!`) /*Condição simples*/
+}
+console.log('Dirija com cinto de segurança')
---
+++ aula11/ex010.js
@@ -0,0 +1,9 @@
+var pais = 'Uruguai'
+
+console.log(`Eu estou no pais ${pais}`)
+ 
+if (pais =='Brasil'){
+    console.log('Voce é Brasileiro')
+}else{
+    console.log('Voce é estrangeiro')
+}

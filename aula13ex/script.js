61578f434f3a71927f2055e9e18e0754a9e9532c
Erick Silva <donizttierick@gmail.com>
2025-09-07 12:48:24 -0300

    .

--- aula13ex/script.js
+++ aula13ex/script.js
@@ -1,7 +1,17 @@
 function carregar(){
-    var msg = document.getElementById('msg')
-    var f = document.getElementById('foto')
-    var data = new Date
-    var hora = data.getHours
-    msg.innerText = (`O seu horario é`)
+    var m = window.document.getElementById('msg')
+    var f = window;document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    m.innerText = (`A sua hora atual é ${hora} horas`)
+    if (hora >=0 && hora < 12){
+        
+        m.innerText = ('Bom dia!!!')
+    }else {
+        if (hora >= 12 && hora < 18){
+           
+            m.innerText = ('Boa tarde!!')
+        }
+            
+    }
 }

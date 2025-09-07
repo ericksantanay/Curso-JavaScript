64d07e5ea1be89f861421a7ac0daf0a237e7fdb4
Erick Silva <donizttierick@gmail.com>
2025-09-07 14:08:32 -0300

    .

---
+++ desafio/script.js
@@ -0,0 +1,8 @@
+function verificar(){
+    var inp1 = window.document.getElementById('itxt1')
+    var inp2 = window.document.getElementById('istxt2')
+    var inp3 = window.document.getElementById('istxt3')
+    var res = window.document.getElementById('result')
+    res.innerText = (`Detectamos`)
+    if (inp1 - )
+}

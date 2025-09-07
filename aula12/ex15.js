1245c83a514f3c3b56215d8f3c1e32dd624ad407
Erick Silva <donizttierick@gmail.com>
2025-09-07 09:59:55 -0300

    .

--- aula12/ex15.js
+++ aula12/ex15.js
@@ -10,20 +10,20 @@ switch(diasem){
         case 1:
             console.log('Segunda')
             break
-            case 2:
-                console.log('Terça')
-                break
-                case 3:
-                    console.log('Quarta')
-                    break
-                    case 4:
-                        console.log('Quinta')
-                        break
-                        case 5:
-                            break
-                            console.log('Sexta')
-                            case 6:
-                                console.log('Sabado')
-                                break
+        case 2:
+            console.log('Terça')
+            break
+        case 3:
+            console.log('Quarta')
+            break
+        case 4:
+            console.log('Quinta')
+            break
+        case 5:
+            break
+            console.log('Sexta')
+        case 6:
+            console.log('Sabado')
+            break
 
 }

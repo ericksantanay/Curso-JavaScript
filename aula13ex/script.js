d215f21eca57435b5c883ce561d9a6e4ad2b9200
Erick Silva <donizttierick@gmail.com>
2025-09-07 12:18:05 -0300

    .

---
+++ aula13ex/script.js
@@ -0,0 +1,7 @@
+function carregar(){
+    var msg = document.getElementById('msg')
+    var f = document.getElementById('foto')
+    var data = new Date
+    var hora = data.getHours
+    msg.innerText = (`O seu horario é`)
+}

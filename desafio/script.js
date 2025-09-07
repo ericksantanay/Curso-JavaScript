fdf38fd6aec469c05ae10b12c983db4698ca1065
Erick Silva <donizttierick@gmail.com>
2025-09-07 14:28:04 -0300

    .

--- desafio/script.js
+++ desafio/script.js
@@ -1,10 +1,11 @@
 function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
     var inp1 = window.document.getElementById('itxt1')
-    var inp2 = window.document.getElementById('istxt2')
-    var inp3 = window.document.getElementById('istxt3')
     var res = window.document.getElementById('result')
-    res.innerText = (`Detectamos ${res}`)
-    if (inp1 - 2025){
-        res.innerText = (`Voce tem ${res}`)
+
+    if (inp1.value.la){
+
     }
-}
+
+    }

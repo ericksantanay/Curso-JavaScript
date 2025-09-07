7f397807bdbe3bd6cb7be2ce639d21ef222f8b6e
Erick Silva <donizttierick@gmail.com>
2025-09-07 09:19:57 -0300

    .

---
+++ aula12/ex14.js
@@ -0,0 +1,13 @@
+var hora = 16
+console.log(`Agora são exatamente ${hora} horas`)
+if (hora <= 12){
+    console.log('Bom dia')
+}else{
+    if(hora <= 18){
+        console.log('Boa tarde')
+    }else{
+        if (hora >= 18){
+            console.log('Boa noite')
+        }
+    }
+}

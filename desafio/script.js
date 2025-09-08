f4da2beb25aa4175aac7b751a303b5eb67034e52
Erick Silva <donizttierick@gmail.com>
2025-09-08 17:45:11 -0300

    .

--- desafio/script.js
+++ desafio/script.js
@@ -3,11 +3,17 @@ function verificar(){
     var ano = data.getFullYear()
     var fano = window.document.getElementById('txtano')
     var res = document.getElementById('result')
-    if (fano <= 16 && fano >= 65){
+    if (fano < 16){
         res.innerText = ('Voce é menor de idade, voce nao vota!!')
     }else{
         if (fano >= 18){
-            res.innerText = ('Seu voto é obrigatatorio')
+            res.innerText = ('Seu voto é obrigata')
+        }else{
+            if (fano >= 16 && fano >= 65){
+                res.innerText = ('Seu voto é opicional')
+            }else{
+                window.alert('[ERRO] Nao foi possivel verificar os dados tente novamente')
+            }
         }
     }
 }

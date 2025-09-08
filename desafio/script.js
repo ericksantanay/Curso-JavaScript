911e3cc717c156503fec8b3023ae35a222ef348c
Erick Silva <donizttierick@gmail.com>
2025-09-08 17:29:29 -0300

    .

--- desafio/script.js
+++ desafio/script.js
@@ -2,12 +2,5 @@ function verificar(){
     var data = new Date()
     var ano = data.getFullYear()
     var fano = window.document.getElementById('txtano')
-    var res = window.document.getElementById('result')
-
-
-    if (fano.value.length == 0 || fano.value > ano){
-        window.alert('[ERRO] Nao foi possivel verificar os dados tente novamente')
-    }else{
-        
-    }
-    }
+    
+}

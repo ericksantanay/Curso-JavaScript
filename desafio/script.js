afcfcfea0c4a0f6cf7d9cda4b1746db51de6b222
Erick Silva <donizttierick@gmail.com>
2025-09-08 17:32:13 -0300

    .

--- desafio/script.js
+++ desafio/script.js
@@ -2,5 +2,6 @@ function verificar(){
     var data = new Date()
     var ano = data.getFullYear()
     var fano = window.document.getElementById('txtano')
+    var res = document.getElementById('result')
     if (ano > )
 }

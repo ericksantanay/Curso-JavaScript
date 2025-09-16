2104916e7cf67383fc43a916ab45694feccb0a62
Erick Silva <donizttierick@gmail.com>
2025-09-16 15:15:35 -0300

    .

--- Curso2/ex02/script.js
+++ Curso2/ex02/script.js
@@ -1,9 +1,9 @@
 var idade = 18
 
-if (idade >= 0 && idade < 16){
+if (idade < 16 ){
     console.log(`Voce tem ${idade} anos, e nao pode votar!!!!`)
 }else{
-    if (idade > 16 || idade < 18 ){
+    if (idade >= 16 || idade < 18  ){
          console.log(`Voce tem ${idade} anos, e seu voto é opicional!!!!`)
     }else {
         if (idade ){

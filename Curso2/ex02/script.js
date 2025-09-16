d133286e5d4f36f60a198a6d2c6ccf1f322e91e7
Erick Silva <donizttierick@gmail.com>
2025-09-16 14:52:19 -0300

    .

--- Curso2/ex02/script.js
+++ Curso2/ex02/script.js
@@ -1,18 +1,3 @@
 var idade = 16
-if (idade <= 16){
-    console.log(`Voce tem ${idade} anos, e voce é menor de idade, voce nao vota!!`)
-}else {
-    if (idade >= 16 && idade < 18 ){
-        console.log(`Voce tem ${idade} anos, seu voto é opicional!!`)
-    }else {
-        if (idade >= 18 ){
-            console.log(`Voce tem ${idade} anos seu voto é obrigatorio!!`)
-        }else{
-            if(idade >= 65 || idade < 110){
-                console.log(`Voce tem ${idade} anos, seu voto é opicional!!`)
-            }else{
-                alert('[ERRO]')  
-            }
-        }
-    }
-}
+
+if {idade >= 0 }

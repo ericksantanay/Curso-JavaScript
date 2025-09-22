8768592d37c96ecf6abe84f8b9ea6aaa6722f345
Erick Silva <donizttierick@gmail.com>
2025-09-22 15:52:19 -0300

    .

---
+++ Curso2/ex004/script.js
@@ -0,0 +1,9 @@
+var valor1 = 10
+var valor2 = 10 
+var soma = valor1 + valor2
+var r = document.getElementById('res')
+
+function soma (valor1,valor2) {
+    return valor1 + valor2
+    r.innerText = (`${soma}`)
+}

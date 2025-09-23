86a5ef1b9e7e9c1012c3aed6c83bb7bddde5fcd6
Erick Silva <donizttierick@gmail.com>
2025-09-23 15:59:31 -0300

    .

--- praticas/15/script.js
+++ praticas/15/script.js
@@ -1,4 +1,11 @@
 function analisar(){
-    var ip = Number(document.getElementById('inum'))
+    var ip = Number(document.getElementById('inum').value)
     var r = document.getElementById('res')
+    var v = 2
+
+    if (ip * v ){
+        r.innerText = ('Esse Número é Par ')
+    }else if (ip ){
+
+    }
 }

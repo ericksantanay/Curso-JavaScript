ba3bcd47ba3bedd549ba51fa4cd418bde66a3fbb
Erick Silva <donizttierick@gmail.com>
2025-09-24 15:29:49 -0300

    Script Pronto

--- praticas/16/script.js
+++ praticas/16/script.js
@@ -1,4 +1,11 @@
 function verifica() {
     var ip = Number(document.getElementById('isenha').value)
     var r = document.getElementById('res')
+    var senha = 1234
+
+    if (ip == senha) {
+        r.innerText = ('Parabéns voce acertou a senha')
+    }else {
+        r.innerText = ('Voce errou!!!')
+    }
 }

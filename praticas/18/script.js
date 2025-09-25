414611528d88f76472dab63ead9ea34afbb0748b
Erick Silva <donizttierick@gmail.com>
2025-09-25 14:42:11 -0300

    Script pronto

--- praticas/18/script.js
+++ praticas/18/script.js
@@ -1,4 +1,16 @@
 function verificar() {
-    var inpu = Number(document.getElementById('iidade').value)
+    var ip = Number(document.getElementById('iidade').value)
     var r = document.getElementById('res')
+
+    if (ip >= 0 && ip < 12) {
+        r.innerText = (`Voce tem ${ip} anos e voce ainda é criança! `)
+    }else if (ip >= 12 && ip < 18) {
+        r.innerText = (`Voce tem ${ip} anos e voce já é adolecente! `)
+    }else if (ip >= 18 && ip < 60) {
+        r.innerText = (`Voce tem ${ip} anos e voce é adulto(a)!!`)
+    }else if (ip >= 60 && ip < 110) {
+        r.innerText = (`Voce tem ${ip} e voce é idoso(a)`)
+    }else if (ip > 110) {
+        alert('[ERRO] Tente novamente mais tarde ')
+    }
 }

95758ed041bbf3a0feced83d9a119cdcb80fb744
Erick Silva <donizttierick@gmail.com>
2025-09-26 20:01:30 -0300

    Ajustes.
    Tem alguns erros que eu ainda vou resolver, eu irei resolver conforme eu for aprendendo mais

--- praticas/20/script.js
+++ praticas/20/script.js
@@ -7,13 +7,13 @@ function TemperaturaQuenteFrio() {
    
     // if, else-if e else.
 
-        if (ip < 20) {
+        if (ip >= -89 && ip < 20) {
             r.innerText = ('Frio!')
         }else if (ip >= 20 && ip < 30) {
             r.innerText = ('Agradavel')
         }else if (ip >= 30) {
             r.innerText = ('Quente')
-        }else {
+        }else{
             alert('[ERRO] TENTE NOVAMENTE.')
         }
 }

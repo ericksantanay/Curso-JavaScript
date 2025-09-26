53ce2ed19cc0e5aefa376ed5918e180a2dfff751
Erick Silva <donizttierick@gmail.com>
2025-09-26 19:48:05 -0300

    .

--- praticas/20/script.js
+++ praticas/20/script.js
@@ -1,5 +1,10 @@
 function TemperaturaQuenteFrio() {
     var ip = Number(document.getElementById('itemp').value)
     var r = document.getElementById('res')
-    
+   
+    // if, else-if e else
+
+        if (ip < 20) {
+            r.innerText = ('Frio!')
+        }
 }

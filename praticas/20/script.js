7b0628143732210cc8d01275ed111b59db354b88
Erick Silva <donizttierick@gmail.com>
2025-09-26 19:50:10 -0300

    Script Pronto

--- praticas/20/script.js
+++ praticas/20/script.js
@@ -6,5 +6,11 @@ function TemperaturaQuenteFrio() {
 
         if (ip < 20) {
             r.innerText = ('Frio!')
+        }else if (ip >= 20 && ip < 30) {
+            r.innerText = ('Agradavel')
+        }else if (ip > 30) {
+            r.innerText = ('Quente')
+        }else {
+            alert('[ERRO] TENTE NOVAMENTE MAIS TARDE.')
         }
 }

dac0d8495fb7b20a934927e7cb380c1d45e0449f
Erick Silva <donizttierick@gmail.com>
2025-09-27 12:56:35 -0300

    Exercicio feito

---
+++ praticas/21 objetos/2/index.js
@@ -0,0 +1,9 @@
+let celular = {
+    marca: "Iphone", 
+        modelo: 14,
+            preço:  2900
+}
+
+console.log(celular["marca"])
+console.log(celular["modelo"])
+console.log(celular["preço"])

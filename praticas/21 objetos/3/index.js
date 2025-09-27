aa2e635c91acc82f7b1ea6d41f81bdae3e83090b
Erick Silva <donizttierick@gmail.com>
2025-09-27 13:23:30 -0300

    Ex Feito
    Usando Objetos e usando o metodo

---
+++ praticas/21 objetos/3/index.js
@@ -0,0 +1,11 @@
+let cachorro = {
+    nome: "kiara",
+    raca: "Pastor Alemão",
+    latir: function() {
+        console.log('au au au au au au auuuuuuuuuuuuuuuuuuuu au au au au au au au au au au au au au au au au au au au au au au au au au au auuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu')
+    }
+}
+
+console.log(cachorro["nome"])
+console.log(cachorro["raca"])
+cachorro.latir()

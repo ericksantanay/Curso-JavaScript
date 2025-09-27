5028f44d2b5f30a7750de752d3d4262eba71943c
Erick Silva <donizttierick@gmail.com>
2025-09-27 14:57:46 -0300

    Pronto

--- praticas/21 objetos/6/script.js
+++ praticas/21 objetos/6/script.js
@@ -3,10 +3,27 @@ var banco = {
     saldo: 20000,
 
     depositar: function() {
+        console.log('Eu quero depositar R$1000')
         saldo = 21000
     },
 
-    sacar: function() {
-        
+    saldos: function() {
+        console.log('21000')
+    },
+
+    sacarValor: function() {
+        console.log('Eu quero sacar R$ 3.000')
+        saldo = 18000
+    },
+
+    verSaldoFinal: function() {
+        console.log('Seu Saldo Final é de 18000' )
     }
 }
+
+console.log(banco.titular)
+console.log(banco.saldo)
+banco.depositar()
+banco.saldos()
+banco.sacarValor()
+banco.verSaldoFinal()

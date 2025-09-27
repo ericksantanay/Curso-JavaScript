f05e2c609aca507fa7cc809ebea49bfa20d1f68b
Erick Silva <donizttierick@gmail.com>
2025-09-27 14:08:17 -0300

    Pronto

--- praticas/21 objetos/4/script.js
+++ praticas/21 objetos/4/script.js
@@ -7,7 +7,7 @@ const cachorro = {
     },
 
     apresenta: function() {
-            console.log(`Meu nome é ${this.nome} e a minha raça é ${this.raça}`) // Eu tenho que por o this. para o js saber o que eu quero mostrar
+        console.log(`Meu nome é ${this.nome} e a minha raça é ${this.raça}`) // Eu tenho que por o this. para o js saber o que eu quero mostrar
         }   
 }
 

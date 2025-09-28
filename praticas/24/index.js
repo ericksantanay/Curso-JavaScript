fe6dde0506f2e0e5b69477ad9000301d495cc93b
Erick Silva <donizttierick@gmail.com>
2025-09-28 15:19:49 -0300

    Tudo Pronto

--- praticas/24/index.js
+++ praticas/24/index.js
@@ -3,11 +3,11 @@ function analisar() {
     let r = document.getElementById('res')
 
     if (ip >= 1 && ip < 12) {
-        r.innerText = (`Voce tem ${ip} anos e é criança, não pode entrar!`)
+        r.innerText = (`Voce é criança, não pode entrar!`)
     }else if (ip >= 12 && ip < 17) {
-        r.innerText = (`Voce tem ${ip} anos e voce é adolescente, pode entrar acompanhado!`)
+        r.innerText = (`Voce é adolescente, pode entrar acompanhado!`)
     }else if (ip >= 18 ) {
-        r.innerText = (`Voce tem ${ip} anos e é adulto, pode entrar na festa!`)
+        r.innerText = (`Voce é adulto, pode entrar na festa!`)
     }else {
         alert('[ERRO] Digite uma idade!')
     }

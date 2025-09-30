a6c9213684c330f52b3e686774ae4edaca681480
Erick Silva <donizttierick@gmail.com>
2025-09-30 12:41:48 -0300

    Ajustes feitos

--- praticas/26/index.js
+++ praticas/26/index.js
@@ -1,10 +1,6 @@
-function calcular() {
-    let ip = Number(document.getElementById('icompra').value)
+function verif() {
+    let ip = Number(document.getElementById('iturno').value)
     let r = document.getElementById('res')
 
-    if (ip >= 100 ) {
-        100 * 0.10 == 100
-        100 - 
-        r.innerText = (`${ip}`)
-    }
+    
 }

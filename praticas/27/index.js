c1deb8b427b2b7ef83980d7602bb0fa77cdc4224
Erick Silva <donizttierick@gmail.com>
2025-09-30 14:42:06 -0300

    Pronto, com alguns erros !

--- praticas/27/index.js
+++ praticas/27/index.js
@@ -1,7 +1,16 @@
 function verif() {
     // Variaveis como o input e o paragrafo que vai sair o resultado
-    let ip = Number(document.getElementById('').value)
+    let ip = Number(document.getElementById('isenha').value)
+    let r = document.getElementById('res')
 
     // Variavel com a senha que eu escolhi
     const senha = 8
+    // if e else-if
+    if (ip > 8) {
+        r.innerText = ('ERROU, NÚMERO É MENOR')
+    }else if (ip < 8 ) {
+        r.innerText = ('ERROU, O NÚMERO É MAIOR')
+    }else if (ip === senha) {
+        r.innerText = ('PARABÉNS VOCE ACERTOU')
+    }
 }

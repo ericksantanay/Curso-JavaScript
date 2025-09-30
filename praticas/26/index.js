f32a0d895f76d5a49cf395b9ec921d5462b1424a
Erick Silva <donizttierick@gmail.com>
2025-09-30 14:04:10 -0300

    Comentarios

--- praticas/26/index.js
+++ praticas/26/index.js
@@ -1,11 +1,14 @@
 function verif() {
+    // Variaveis do input e do paragrafo que vai sair o resultado.
     let ip = document.getElementById('iturno').value 
     let r = document.getElementById('res')
 
+    // Variaveis pré definidas!
     const dia1 = 'D'
     const tarde = 'T';
     const noite = 'N';
 
+    // If, else-if e else.
     if (ip == dia) {
         r.innerText = (`Turno da manhã`)
     }else if (ip == tarde) {

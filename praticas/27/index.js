bccc9fc076fac274eaa4ec569cea051ff785153e
Erick Silva <donizttierick@gmail.com>
2025-09-30 14:33:59 -0300

    Primeira parte pronta

---
+++ praticas/27/index.js
@@ -0,0 +1,7 @@
+function verif() {
+    // Variaveis como o input e o paragrafo que vai sair o resultado
+    let ip = Number(document.getElementById('').value)
+
+    // Variavel com a senha que eu escolhi
+    const senha = 8
+}

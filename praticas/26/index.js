ef12c96eaa34b5ac9b82eac0222c811c372e45b8
Erick Silva <donizttierick@gmail.com>
2025-09-30 14:26:36 -0300

    Script pronto com ajustes

--- praticas/26/index.js
+++ praticas/26/index.js
@@ -4,17 +4,20 @@ function verif() {
     let r = document.getElementById('res')
 
     // Variaveis pré definidas!
-    const dia = 'D'
+    const dia = 'M'
     const tarde = 'T';
     const noite = 'N';
 
     // If, else-if e else.
     if (ip == dia) {
         r.innerText = (`Turno da manhã`)
+        document.body.style.backgroundColor = 'rgb(15, 125, 216)'
     }else if (ip == tarde) {
         r.innerText = (`Turno da Tarde`)
+        document.body.style.backgroundColor = 'rgb(216, 109, 15)'
     }else if ( ip == noite) {
         r.innerText = (`Turno da noite`)
+        document.body.style.backgroundColor = 'rgb(41, 48, 46)'
     }else {
        alert('[ERRO] Escreva no campo abaixo com umas das letras em letras maiusculo.')
     }

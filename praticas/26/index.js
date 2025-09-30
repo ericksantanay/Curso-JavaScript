f1721d325e06633c449805e5a1217275c1de62ac
Erick Silva <donizttierick@gmail.com>
2025-09-30 12:39:33 -0300

    Ajuste

---
+++ praticas/26/index.js
@@ -0,0 +1,10 @@
+function calcular() {
+    let ip = Number(document.getElementById('icompra').value)
+    let r = document.getElementById('res')
+
+    if (ip >= 100 ) {
+        100 * 0.10 == 100
+        100 - 
+        r.innerText = (`${ip}`)
+    }
+}

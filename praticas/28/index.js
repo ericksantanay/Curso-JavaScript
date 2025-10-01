db51f555f413319cc0956174e0cfd1a9b1103ba1
Erick Silva <donizttierick@gmail.com>
2025-10-01 18:02:05 -0300

    .

--- praticas/28/index.js
+++ praticas/28/index.js
@@ -2,13 +2,13 @@ function mostrar() {
     let ip = Number(document.getAnimations('iest').value)
     let r = document.getElementById('res')
 
-    if (ip == 12,1,2) {
+    if (ip == 12 || ip == 1 || ip == 2) {
         r.innerText = ('Verão')
-    }else if (ip == 3,4,5) {
+    }else if (ip == 3 || ip == 4 || ip == 5) {
         r.innerText = ('Outono')
-    }else if (ip == 6,7,8) {
+    }else if (ip == 6 || ip == 7 || ip == 8) {
         r.innerText ('Inverno')
-    }else if (ip == 9,10,11) {
+    }else if (ip == 9 || ip == 10 || ip == 11) {
         r.innerText = ('Primavera')
     }else {
         alert('[ERRO]')

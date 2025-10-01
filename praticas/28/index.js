5c390c86774235b7849d730dcee22e16c41823c9
Erick Silva <donizttierick@gmail.com>
2025-10-01 18:04:34 -0300

    Finalizado

--- praticas/28/index.js
+++ praticas/28/index.js
@@ -1,5 +1,5 @@
 function mostrar() {
-    let ip = Number(document.getAnimations('iest').value)
+    let ip = Number(document.getElementById('iest').value)
     let r = document.getElementById('res')
 
     if (ip == 12 || ip == 1 || ip == 2) {
@@ -7,7 +7,7 @@ function mostrar() {
     }else if (ip == 3 || ip == 4 || ip == 5) {
         r.innerText = ('Outono')
     }else if (ip == 6 || ip == 7 || ip == 8) {
-        r.innerText ('Inverno')
+        r.innerText = ('Inverno')
     }else if (ip == 9 || ip == 10 || ip == 11) {
         r.innerText = ('Primavera')
     }else {

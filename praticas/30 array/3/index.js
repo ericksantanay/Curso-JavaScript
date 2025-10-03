c8cd23e5e6613454d0ac6250d32d093eb381ee9c
Erick Silva <donizttierick@gmail.com>
2025-10-03 18:02:59 -0300

    Pronto

--- praticas/30 array/3/index.js
+++ praticas/30 array/3/index.js
@@ -1,7 +1,5 @@
-
               //0    //1  //2 //3 //4 //5  //6 //7 //8  //9
 const numero = [1,    2,   3,  4,  5,  6,  7,  8,  9,  10]
-
 console.log(numero[1])
 console.log(numero[3])
 console.log(numero[5])

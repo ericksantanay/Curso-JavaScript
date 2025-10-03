b8cd358ea16a4ae991319bbc9aad485d56f21732
Erick Silva <donizttierick@gmail.com>
2025-10-03 18:41:04 -0300

    .

--- praticas/30 array/4/index.js
+++ praticas/30 array/4/index.js
@@ -1,4 +1,8 @@
 const nomes = ["Oi", "Bão?", "BLZ", "Joia?", "Suave", "Bom dia"]
 
 let r = document.getElementById('res')
-
+let indice = 0
+r.innerText = (`${nomes[indice]}`)
+setInterval(() => {
+    indice++
+}, 2000);

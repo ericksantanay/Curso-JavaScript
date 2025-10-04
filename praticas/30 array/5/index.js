6341cf528344cd27e29b44b7319373066d883fb9
Erick Silva <donizttierick@gmail.com>
2025-10-04 15:26:14 -0300

    Adiconando comentarios

--- praticas/30 array/5/index.js
+++ praticas/30 array/5/index.js
@@ -5,6 +5,7 @@ const pessoa = ["Erick", "Santana", 20, "Iniciante"]//Array
 //r.innerHTML = pessoa.join("-")
 //pessoa.push("Qulquer coisa")
 //pessoa.shift()
+//pessoa.unshift("È os Guri né pai.")
 r.innerHTML = pessoa
 
 
@@ -17,22 +18,26 @@ r.innerHTML = pessoa
   /*
     Os indice mudam.
 
-  1- Metodo (Join)
+  1- Metodo (join)
   Com esse metodo eu posso adicionar qualquer coisa que ele vai adicionar entre os espaços de uma plavra a outra.     
     Ex:  r.innerHTML = pessoa.join("-")
 -------------------------------------------------------------------------------------------------------------
-2- Metodo (Pop) Esse remove oultimo item
+2- Metodo (pop) Esse remove oultimo item
    Com esse metodo eu consigo remover o ultimo item de uma lista no caso o (array).
     Ex: pessoa.pop() 
     Um detalhe eu tenho que deixar o pessoa.pop antes o innerHTMl ou do innerText.
 -------------------------------------------------------------------------------------------------------------
-3- Metodo (Push) Esse adiciona
+3- Metodo (push) Esse adiciona
     Com esse metodo eu consigo adicionar alguma coisa na lista.
     Ex: pessoa.push("Qulquer coisa")
 -------------------------------------------------------------------------------------------------------------
-4- Metodo (Shift) Esse metodo remove o primeiro item
+4- Metodo (shift) Esse metodo remove o primeiro item
     Com esse metodo eu removo o primeiro item da lista no caso o (array)
     Ex: pessoa.shift()
 -------------------------------------------------------------------------------------------------------------
-
+5- Metodo (unshift)
+    Com esse metodo eu adiciono alguma coisa no primeiro item da minha lista (array)
+    Ex: pessoa.unshift("Qualquer coisa")
+-------------------------------------------------------------------------------------------------------------
+6- 
   */

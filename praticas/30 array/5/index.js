969c7ef51b88e818a326276b6bf698ff74f4d398
Erick Silva <donizttierick@gmail.com>
2025-10-04 15:16:25 -0300

    .

--- praticas/30 array/5/index.js
+++ praticas/30 array/5/index.js
@@ -3,6 +3,8 @@ const pessoa = ["Erick", "Santana", 20, "Iniciante"]//Array
 
 //pessoa.pop()
 //r.innerHTML = pessoa.join("-")
+//pessoa.push("Qulquer coisa")
+//pessoa.shift()
 r.innerHTML = pessoa
 
 
@@ -18,11 +20,16 @@ r.innerHTML = pessoa
   Com esse metodo eu posso adicionar qualquer coisa que ele vai adicionar entre os espaços de uma plavra a outra.     
     Ex:  r.innerHTML = pessoa.join("-")
 -------------------------------------------------------------------------------------------------------------
-2- Metodo (Pop)
+2- Metodo (Pop) Esse remove oultimo item
    Com esse metodo eu consigo remover o ultimo item de uma lista no caso o (array).
     Ex: pessoa.pop() 
     Um detalhe eu tenho que deixar o pessoa.pop antes o innerHTMl ou do innerText.
 -------------------------------------------------------------------------------------------------------------
-3- Metodo (Push)
-    Com esse metodo eu consigo adicionar 
+3- Metodo (Push) Esse adiciona
+    Com esse metodo eu consigo adicionar alguma coisa na lista.
+    Ex: pessoa.push("Qulquer coisa")
+-------------------------------------------------------------------------------------------------------------
+4- Metodo (Shift) Esse metodo remove o primeiro item
+    Com esse metodo eu removo o primeiro item da lista no caso o (array)
+    Ex: pessoa.shift()
   */

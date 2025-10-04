596835777d546cc604acbd491d2364b3f64547ad
Erick Silva <donizttierick@gmail.com>
2025-10-04 16:57:49 -0300

    Pronto

--- praticas/30 array/5/index.js
+++ praticas/30 array/5/index.js
@@ -8,8 +8,15 @@ const  lista3 = ["as", "pera", "Àgsssua", "Carnsssse"]
 
 const jogadores = ["Pipi", "Zeze", "Pele", "Neymar", "Ronaldo", "Messi", "Cristiano Ronaldo", "Wilian"]
 
-const numeros = [1, 90, 50, 60, 23, 2, 45, 100, 10]
 
+/*
+const numeros = [1, 90, 50, 60, 23, 2, 45, 100, 10]
+const maior20 = numeros.filter(filtragem)
+function filtragem(value, index, array) {
+    return value > 20
+}
+r.innerHTML = maior20
+*/
 //const craques = jogadores.slice(2, 6)
 //const superLista = lista1.concat(lista2, lista3)
 //pessoa.pop()
@@ -21,8 +28,20 @@ const numeros = [1, 90, 50, 60, 23, 2, 45, 100, 10]
 //pessoa.splice(1,3, "Item adicionado1")
 //jogadores.sort()
 //jogadores.reverse()
-numeros.sort(function(a,b){return a-b})
-r.innerHTML = numeros
+//numeros.sort(function(a,b){return a-b})
+//numeros.sort(function(a,b){return b-a})
+
+/*
+function MaiorNumero(array){
+    return Math.max.apply(null, array)
+}
+*/
+
+/*
+ function MenorNumero(array){
+        return Math.min.apply(null, array)
+    }
+*/
 
 
   /*
@@ -98,17 +117,38 @@ r.innerHTML = numeros
 -------------------------------------------------------------------------------------------------------------
 13 Método para deixar o número do maior para o menor (sort)
 
+    Usa o mesmo raciocinio do outro mas muda o a e o b
+    Ex: numeros.sort(function(a,b){return b-a})
+-------------------------------------------------------------------------------------------------------------
+14- Metodo para pegar um número maximo dentro de uma lista (array)
 
+    Para esse metodo eu preciso fazer uma função exemplo asseguir.
 
+    Ex: function MaiorNumero(array){
+        return Math.max.apply(null, array)
+    }
+    r.innerHTML = MaiorNumero(numeros) No InnerHTML eu tenho que por isso.
+-------------------------------------------------------------------------------------------------------------
+15- Método para pegar o menor número.
+    
+    È o mesmo conceito do anterior mas muda algumas coisas.
+    Para por um número menor é só trocar o (max) por (min)
 
+    Ex: function MenorNumero(array) {
+        return Math.min.apply(null, array)
+       }
+    r.innerHTML = MenorNumero(numeros)
+-------------------------------------------------------------------------------------------------------------
+16- Filtrar os números.
 
+    Eu ecolho os números que eu quero filtrar e eu filtro com essa funçãozinha.
+    Ex: const numeros = [1, 90, 50, 60, 23, 2, 45, 100, 10]
+    const maior20 = numeros.filter(filtragem)
+    function filtragem(value, index, array) {
+    return value > 20
+    }
+    r.innerHTML = maior20
 
-
-
-
-
-
-
-
+-------------------------------------------------------------------------------------------------------------
     Com alguns metodos eu preciso criar um variavel e colocar o nome da variavel no innerText ou innerHTML e por o metodo dentro dessa variavel
   */

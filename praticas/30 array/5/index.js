99b78897c343c499675d882e3140def3c86cd858
Erick Silva <donizttierick@gmail.com>
2025-10-04 14:53:35 -0300

    .

--- praticas/30 array/5/index.js
+++ praticas/30 array/5/index.js
@@ -4,4 +4,10 @@ const pessoa = ["Erick", "Santana", 20, "Iniciante"]
 
   //r.innerHTML = pessoa.join("-")
 
-  // 1
+  /*
+  
+  1- Metodo (Join)
+  Com esse metodo eu posso adicionar qualquer coisa que ele vai adicionar entre os espaços de uma plavra a outra.     
+       r.innerHTML = pessoa.join("-")
+  
+  */

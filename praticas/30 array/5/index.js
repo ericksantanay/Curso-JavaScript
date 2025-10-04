fa1c4fa29c1d2dcdd2b15c46e945efeb3c7c15de
Erick Silva <donizttierick@gmail.com>
2025-10-04 14:59:53 -0300

    .

--- praticas/30 array/5/index.js
+++ praticas/30 array/5/index.js
@@ -1,13 +1,17 @@
 const r = document.getElementById('res')
 const pessoa = ["Erick", "Santana", 20, "Iniciante"]
 
-
+pessoa.pop()
   //r.innerHTML = pessoa.join("-")
 
   /*
   
   1- Metodo (Join)
   Com esse metodo eu posso adicionar qualquer coisa que ele vai adicionar entre os espaços de uma plavra a outra.     
-       r.innerHTML = pessoa.join("-")
+    Ex:  r.innerHTML = pessoa.join("-")
+-------------------------------------------------------------------------------------------------------------
+2- Metodo (Pop)
+   Com esse metodo eu consigo remover o ultimo item de uma lista no caso o (array).
+    Ex: 
   
   */

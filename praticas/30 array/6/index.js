0cbf5e09dd18e8c56263197e68fce869c3edeb82
Erick Silva <donizttierick@gmail.com>
2025-10-04 17:55:36 -0300

    Exercicio terminado

---
+++ praticas/30 array/6/index.js
@@ -0,0 +1,26 @@
+const r = document.getElementById('res')
+
+/* Exercicios 1
+
+const alimentos = ["Maça", "Banana", "Beterraba", "Pera", "Pão"]
+alimentos.push("Queijo")
+alimentos.shift()
+r.innerHTML = alimentos.join("...")
+*/
+
+/*
+Exercicio 2
+const jogadores = ["Pelé", "Maradona", "Neymar", "Messi", "Galo"]
+jogadores.sort()
+r.innerHTML = jogadores
+*/
+
+/*Exercicio 3
+
+const notas = [10, 3, 7, 9, 5, 2]
+const maior = notas.filter(Filtro)
+function Filtro(value, index, array) {
+    return value > 6
+}
+r.innerHTML = maior
+*/

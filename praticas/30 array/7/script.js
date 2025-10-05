f82e725d63b367029d47b458f0457447a5b68be4
Erick Silva <donizttierick@gmail.com>
2025-10-05 13:34:53 -0300

    .

--- praticas/30 array/7/script.js
+++ praticas/30 array/7/script.js
@@ -2,7 +2,6 @@ const r = document.getElementById('res')
 
 const cardapio = ["Hambúrguer", "Batata", "Refrigerante", "Sobremesa"]
 
-
 cardapio.push("Cachorro-Quente")
 cardapio.shift()
 r.innerText = cardapio.join(",")

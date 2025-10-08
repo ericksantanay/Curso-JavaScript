c10b073ee242c09767689332a0235ebb7221a043
Erick Silva <donizttierick@gmail.com>
2025-10-08 16:18:15 -0300

    Pronto

--- pratica2/1/script.js
+++ pratica2/1/script.js
@@ -1,10 +1,11 @@
 function adicionar() {
     const ip = document.getElementById('itxt').value
-   
-    function adicionarTexto() {
+    let marcar = ''
+    
         let p = document.getElementById('res')
         p.append(`${ip}`)
-    }
+        marcar++
+    
 
     if (ip == '') {
         alert('ERRO')

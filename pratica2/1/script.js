f4a6cec466f5fa06075cccd8f178c903b3a7eb4d
Erick Silva <donizttierick@gmail.com>
2025-10-08 16:14:48 -0300

    .

--- pratica2/1/script.js
+++ pratica2/1/script.js
@@ -1,3 +1,12 @@
 function adicionar() {
-    
+    const ip = document.getElementById('itxt').value
+   
+    function adicionarTexto() {
+        let p = document.getElementById('res')
+        p.append(`${ip}`)
+    }
+
+    if (ip == '') {
+        alert('ERRO')
+    }
 }

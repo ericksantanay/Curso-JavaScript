8f557e380fbac36ba9fb848899dcd71c4dffbf43
Erick Silva <donizttierick@gmail.com>
2025-10-11 19:50:20 -0300

    Pronto

--- pratica2/6/script.js
+++ pratica2/6/script.js
@@ -1,7 +1,15 @@
-function texto() {
+    let indice = [0] 
+
+
+    function texto() {
     const r = document.getElementById('res')
     let textos = ["Ola Boniteza", "Ola Lindo(a)", "Celular", "Pão", "Pão de queijo", "Mortadela", "Truco", "Uno", "Fusca", "Gol", "Celular", "Mouse", "Teclado"]
+    
+    
+    r.innerText = (textos[indice])
+    indice++
 
-
-    textos++
+    if (indice >= textos.length) {
+        indice = 0
+    }
 }

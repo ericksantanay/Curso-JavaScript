dcafcce57b744457e02891cad7a2d1bdb60876e3
Erick Silva <donizttierick@gmail.com>
2025-10-11 18:03:32 -0300

    PRONTO

--- pratica2/5/script.js
+++ pratica2/5/script.js
@@ -1,3 +1,11 @@
 function azul() {
-    
+    document.body.style.backgroundColor = 'blue'
+}
+
+function vermelho() {
+    document.body.style.backgroundColor = 'red'
+}
+
+function verde() {
+    document.body.style.backgroundColor = 'green'
 }

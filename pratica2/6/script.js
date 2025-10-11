2fc997676559a74fb736a817e33304beba884660
Erick Silva <donizttierick@gmail.com>
2025-10-11 18:13:25 -0300

    .

---
+++ pratica2/6/script.js
@@ -0,0 +1,7 @@
+function texto() {
+    const r = document.getElementById('res')
+    let textos = ["Ola Boniteza", "Ola Lindo(a)", "Celular", "Pão", "Pão de queijo", "Mortadela", "Truco", "Uno", "Fusca", "Gol", "Celular", "Mouse", "Teclado"]
+
+
+    textos++
+}

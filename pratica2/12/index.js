03623bbbd60df678a70161acb875037763231360
Erick Silva <donizttierick@gmail.com>
2025-10-13 18:58:26 -0300

    Pronto

---
+++ pratica2/12/index.js
@@ -0,0 +1,11 @@
+function mudouAzul() {
+    document.body.style.backgroundColor = 'blue'
+}
+
+function mudouVermelho() {
+    document.body.style.backgroundColor = 'red'
+}
+
+function mudouVerde() {
+    document.body.style.backgroundColor = 'green'
+}

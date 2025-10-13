4d673e651f5ceaca1024fefc18b66afbd56518e5
Erick Silva <donizttierick@gmail.com>
2025-10-13 17:56:59 -0300

    Pronto

--- pratica2/11/index.js
+++ pratica2/11/index.js
@@ -1,7 +1,11 @@
-contador = 0
-function Clicou() {
+    contador = 0
+    function Clicou() {
     const r = document.getElementById('res')
     
     contador++
     r.innerText = (`${contador}`)
+
+    if (contador == 100) {
+        r.innerText = ('Parabens voce chegou no 100')
+    }
 }

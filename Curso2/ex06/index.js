52240e5fb2dda663b8f7d444c52ee0ee09b26c18
Erick Silva <donizttierick@gmail.com>
2025-10-14 15:03:00 -0300

    Pronto

---
+++ Curso2/ex06/index.js
@@ -0,0 +1,10 @@
+
+class Carro {
+    constructor(valor1,valor2,valor3) {
+        this.marca = valor1
+        this.modelo = valor2
+        this.ano = valor3
+    }
+}
+
+const uno = new Carro("Fiat", "Uno", 2001)

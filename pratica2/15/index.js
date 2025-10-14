7b09699a49875812d1ae68bcc6d916f704c2e69c
Erick Silva <donizttierick@gmail.com>
2025-10-14 15:53:08 -0300

    .

--- pratica2/15/index.js
+++ pratica2/15/index.js
@@ -2,12 +2,14 @@
     function iniciar() {
 
     tempo = setInterval(function(){
+        const re = document.getElementById('resu')
         const r = document.getElementById('res')
-        r.innerText = (`${contador}`)
+        r.innerText = (`${contador} s`)
         contador++
 
         if (contador == 60) {
-            r.innerText = (``)
+            r.innerText = (`${contador}`)
+            re.innerText = (`${contador}`)
         }
     }, 1000)
 }

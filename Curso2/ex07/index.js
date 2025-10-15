e38406d762d0944bd728445ffbbca366dd328fdc
Erick Silva <donizttierick@gmail.com>
2025-10-15 10:40:43 -0300

    .

--- Curso2/ex07/index.js
+++ Curso2/ex07/index.js
@@ -1,3 +1,4 @@
+// Pega o dia 
 let data = new Date()
 console.log(data)
 
@@ -11,3 +12,8 @@ console.log(ano)
 // Pegar o mes de 0 até 11 sendo 0 janeiro e 11 dezembro
 let mes = data.getMonth()
 console.log(mes)
+
+const mesesDoAno = ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"];
+
+let mesEscrito = mesesDoAno[data.getMonth()]
+console.log(mesEscrito)

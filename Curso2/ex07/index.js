19e76930547d280d1aea21e62af637c881121f12
Erick Silva <donizttierick@gmail.com>
2025-10-15 10:33:03 -0300

    .

--- Curso2/ex07/index.js
+++ Curso2/ex07/index.js
@@ -1,8 +1,13 @@
 let data = new Date()
-
+console.log(data)
 
 // Para pegar o ano eu faço o seguinte
 
+// Esse pega o ano atual com 4 digitos
 let ano = data.getFullYear()
-
 console.log(ano)
+
+
+// Pegar o mes de 0 até 11
+let mes = data.getMonth()
+console.log(mes)

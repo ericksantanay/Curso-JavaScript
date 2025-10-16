d50b027e06f16ea4adab5a3e7723615f8374b217
Erick Silva <donizttierick@gmail.com>
2025-10-16 16:07:38 -0300

    Relogio feito

--- pratica2/20/script.js
+++ pratica2/20/script.js
@@ -6,5 +6,15 @@ setInterval(function(){
     let s = document.getElementById('seg')
 
     let data = new Date()
-    
+    // Hora
+    let hora = data.getHours()
+    h.innerText = (hora)
+
+    // Minutos
+    let minutos = data.getMinutes()
+    m.innerText = (minutos)
+
+    // Segundos
+    let segundos = data.getSeconds()
+    s.innerText = (segundos)
 }, 1000)

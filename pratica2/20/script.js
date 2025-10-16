af74a1900f02477de9a78e157bb28166785059fa
Erick Silva <donizttierick@gmail.com>
2025-10-16 16:05:14 -0300

    .

---
+++ pratica2/20/script.js
@@ -0,0 +1,10 @@
+
+
+setInterval(function(){
+    let h = document.getElementById('hora')
+    let m = document.getElementById('min')
+    let s = document.getElementById('seg')
+
+    let data = new Date()
+    
+}, 1000)

8bdd4cb022ed37d042dd49ebad39d70ba2c59480
Erick Silva <donizttierick@gmail.com>
2025-10-16 16:17:54 -0300

    Pronto

--- pratica2/20/script.js
+++ pratica2/20/script.js
@@ -1,6 +1,24 @@
-function gerarPalavra() {
-    let mensaguem = document.getElementById('res-data')
+    indice = 0
+    function gerarPalavra() {
+    let palavras =  ["Hoje é um bom dia para aprender algo novo!",
+    "A sorte sorri pra quem não desiste.",
+    "Você está mais perto do sucesso do que imagina!",
+    "Confie no processo e no seu código!",
+    "A persistência vence o talento.",
+    "Hoje é o dia de colocar suas ideias em prática!"]
+    indice++
+
+    let mensaguem = document.getElementById('mens')
+    let Data = document.getElementById('res-data')
     
+    if (indice == 5) {
+        indice = 0
+    }
+    mensaguem.innerText = (palavras[indice])
+
+    let dia = new Date()
+    let dataBr = dia.toLocaleString ('pt-BR')
+    Data.innerText = (dataBr)
 }
 
 setInterval(function(){

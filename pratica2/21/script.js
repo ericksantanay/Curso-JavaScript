824a85b2fb39afa497a873089e2170216d126ae5
Erick Silva <donizttierick@gmail.com>
2025-10-17 20:53:34 -0300

    .

--- pratica2/21/script.js
+++ pratica2/21/script.js
@@ -1,4 +1,14 @@
     let perguntas = document.getElementById('perguntas')
+    indice = 0
     function verificarResposta1() {
-
+    let pergunta = [
+    {
+    pergunta: "Qual linguagem é usada para estilizar páginas da web?",
+    opcoes: ["HTML", "CSS", "JavaScript"],
+    correta: 1 // índice da resposta certa (CSS)
+  },
+    ]
+    indice++
+    perguntas.innerText = (pergunta[indice])
+    
 }

802907528dc3fb6093ec5921a2b8a96ba72533af
Erick Silva <donizttierick@gmail.com>
2025-10-18 19:29:39 -0300

    .

--- pratica2/23/script.js
+++ pratica2/23/script.js
@@ -1,12 +1,17 @@
     
-    // RELOGIO QUE MOSTRA AS HORAS 
+    // RELOGIO
     setInterval(function(){
     let dia = new Date()
     let h = document.getElementById('hora')
     let m = document.getElementById('min')
     let s = document.getElementById('seg')
-
+    
+    // HORAS
     let hora = dia.getHours()
     h.innerText = (hora)
+
+    // MINUTOS
+    let minuto = dia.getMinutes()
+    m.innerText = (minuto)
    
 }, 1000)

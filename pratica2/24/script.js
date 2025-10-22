b547c6ca3a2c9a0046283208b66e7f5cfd29e8f3
Erick Silva <donizttierick@gmail.com>
2025-10-22 17:58:05 -0300

    .

--- pratica2/24/script.js
+++ pratica2/24/script.js
@@ -2,6 +2,12 @@ let tempo = document.getElementById('tempo')
 
     let data = new Date()
     let hora = data.getHours()
+    if (hora >= 5 && hora < 12 ) {
+        tempo.innerText = ('BOM DIA!!')
+    }else if (hora > 12 || hora < 18)
+
+
+
 
 let nome = document.getElementById('ipnome').value
 

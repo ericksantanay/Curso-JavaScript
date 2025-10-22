710d90e9273c2cfdfa2bd7cf1bf9466d365d562a
Erick Silva <donizttierick@gmail.com>
2025-10-22 17:56:42 -0300

    .

--- pratica2/24/script.js
+++ pratica2/24/script.js
@@ -1,5 +1,8 @@
 let tempo = document.getElementById('tempo')
 
+    let data = new Date()
+    let hora = data.getHours()
+
 let nome = document.getElementById('ipnome').value
 
 let r = document.getElementById('saudacao')

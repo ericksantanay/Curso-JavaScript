702e238af3c128f7a53b21d1644530eb545f3bff
Erick Silva <donizttierick@gmail.com>
2025-10-24 18:48:45 -0300

    .

---
+++ pratica2/25/script.js
@@ -0,0 +1,10 @@
+// CONTADOR 1, PARTE QUE ADICIONA
+    contador1 = 0
+function aumentar1() {
+    let res1 = document.getElementById('unitenis')
+    res1.innerText = (`Unidades ${contador1}`)
+    contador1++
+}
+
+
+// CONTADOR 1, PARTE QUE DIMINUI 

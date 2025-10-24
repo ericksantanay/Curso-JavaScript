56474177c901a66ee7e1f38630f277721fc4d0ed
Erick Silva <donizttierick@gmail.com>
2025-10-24 19:10:05 -0300

    .

--- pratica2/25/script.js
+++ pratica2/25/script.js
@@ -1,6 +1,6 @@
 // CONTADOR 1, PARTE QUE ADICIONA
     let res1 = document.getElementById('unitenis')
-    contador1 = 10
+    contador1 = 11
 function aumentar1() {
     res1.innerText = (`Unidades ${contador1}`)
     contador1++
@@ -8,12 +8,34 @@ function aumentar1() {
 
 
 // CONTADOR 1, PARTE QUE DIMINUI 
-    contador1 = 10
+    contador1 = 9
     function diminuir1() {
     res1.innerText = (`Unidades ${contador1}`)
     contador1--
 
-    if (contador1 == 0 ) {
-        document.body.style.color = 'red'
+    if (contador1 == -1 ) {
+        contador1 = 0
+        res1.innerText = ('⚠️ Produto esgotado!')
     } 
 }
+
+
+// FUNÇÃO 2  ADICIONAR 
+    let res2 = document.getElementById('unicalca')
+    contador2 = 10
+    function aumentar2() {
+    res2.innerText = (`Unidades ${contador2}`)
+    contador2++
+    }
+
+    // FUNÇÃO 2 DIMINUIR
+        contador2 = 9
+    function diminuir2() {
+        res2.innerText = (`Unidades ${contador2}`)
+        contador2--
+
+        if (contador2 == -1) {
+            contador = 0
+            res2.innerText = ('⚠️ Produto esgotado!')
+        }
+    }

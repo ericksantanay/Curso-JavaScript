740ff02fb010b72ef3804221a75684b905482c3f
Erick Silva <donizttierick@gmail.com>
2025-10-26 15:40:08 -0300

    .

--- pratica2/27/script.js
+++ pratica2/27/script.js
@@ -1,11 +1,16 @@
     const temp = document.getElementById('temperatura')
     const forn = document.getElementById('forno')
-    
+
     indice = 0
     function aumentarTemp() {
+
         indice++
         temp.innerText = (`A TEMPERATURA É ${indice} GRAUS`)
-    }
+
+}
+
+
+
 
 
     function diminuirTemp() {
@@ -13,6 +18,17 @@
         temp.innerText = (`A TEMPERATURA É ${indice} GRAUS`)
     }
 
-    if (indice < 100) {
-        
-    }
+   
+        if (indice < 100) {
+        forn.innerText = ('O FORNO ESTA FRIO!')
+        document.body.style.backgroundColor = 'blue'
+        document.body.style.color = 'white'
+    }else if (indice >= 100 && indice < 200) {
+        forn.innerText = ('TEMPERATURA IDEAL!')
+        document.body.style.backgroundColor = 'yellow'
+        document.body.style.color = 'black'
+    }else if (indice > 200){
+        forn.innerText = ('CUIDADO! ESTÁ MUITO QUENTE!')
+        document.body.style.backgroundColor = 'red'
+        document.body.style.color = 'white'
+    } 

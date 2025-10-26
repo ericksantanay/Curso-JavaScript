d8e5ab95cd901833dd3d4afb0694a94cf1b2ef16
Erick Silva <donizttierick@gmail.com>
2025-10-26 15:01:17 -0300

    .

--- pratica2/27/script.js
+++ pratica2/27/script.js
@@ -1,1 +1,10 @@
     const temp = document.getElementById('temperatura')
+
+    indice = 0
+    function aumentarTemp() {
+        indice++
+        temp.innerText = (indice)
+    }
+
+
+    function 

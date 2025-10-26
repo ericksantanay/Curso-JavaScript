243e82f2ee989f1a079e7657ea78b62ec3fb93a9
Erick Silva <donizttierick@gmail.com>
2025-10-26 15:42:12 -0300

    .

--- pratica2/27/script.js
+++ pratica2/27/script.js
@@ -3,22 +3,9 @@
 
     indice = 0
     function aumentarTemp() {
-
         indice++
         temp.innerText = (`A TEMPERATURA É ${indice} GRAUS`)
-
-}
-
-
-
-
-
-    function diminuirTemp() {
-        indice--
-        temp.innerText = (`A TEMPERATURA É ${indice} GRAUS`)
-    }
-
-   
+        
         if (indice < 100) {
         forn.innerText = ('O FORNO ESTA FRIO!')
         document.body.style.backgroundColor = 'blue'
@@ -32,3 +19,26 @@
         document.body.style.backgroundColor = 'red'
         document.body.style.color = 'white'
     } 
+}
+
+
+    function diminuirTemp() {
+        indice--
+        temp.innerText = (`A TEMPERATURA É ${indice} GRAUS`)
+        
+        if (indice < 100) {
+        forn.innerText = ('O FORNO ESTA FRIO!')
+        document.body.style.backgroundColor = 'blue'
+        document.body.style.color = 'white'
+    }else if (indice >= 100 && indice < 200) {
+        forn.innerText = ('TEMPERATURA IDEAL!')
+        document.body.style.backgroundColor = 'yellow'
+        document.body.style.color = 'black'
+    }else if (indice > 200){
+        forn.innerText = ('CUIDADO! ESTÁ MUITO QUENTE!')
+        document.body.style.backgroundColor = 'red'
+        document.body.style.color = 'white'
+    } 
+    }
+
+   

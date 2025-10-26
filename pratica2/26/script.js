0c0b794cc51c294486a3d196f565200a93f55101
Erick Silva <donizttierick@gmail.com>
2025-10-26 14:48:22 -0300

    Ajustes

--- pratica2/26/script.js
+++ pratica2/26/script.js
@@ -3,8 +3,9 @@
     contador1 = 0
     function pao1() {
         let res1 = document.getElementById('resultado1')
-        res1.innerText = (`Votos ${contador1}`)
         contador1++
+        res1.innerText = (`Votos ${contador1}`)
+        
 
         if (contador1 > 30) {
             res1.innerText = (`Votos 30`)
@@ -18,10 +19,11 @@
     contador2 = 0
     function pao2() {
         let res2 = document.getElementById('resultado2')
-        res2.innerText = (`Votos ${contador2}`)
         contador2++
+        res2.innerText = (`Votos ${contador2}`)
+        
 
-        if (contador2 = 30) {
+        if (contador2 > 30) {
             res2.innerText = (`Votos 30`)
              restotal.innerText = (`O GANHADOR FOI O PÃO DE QUEIJO COM ${contador2} VOTOS`)
         }
@@ -32,8 +34,9 @@
     contador3 = 0
     function pao3() {
         let res3 = document.getElementById('resultado3')
-        res3.innerText = (`Votos ${contador3}`)
         contador3++
+        res3.innerText = (`Votos ${contador3}`)
+        
 
         if (contador3 > 30) {
             res3.innerText = (`Votos 30`)

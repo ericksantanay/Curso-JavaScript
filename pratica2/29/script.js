3338fbf978810145b6048ebdba2884ac1fe9e41b
Erick Silva <donizttierick@gmail.com>
2025-10-27 14:25:12 -0300

    .

--- pratica2/29/script.js
+++ pratica2/29/script.js
@@ -1,5 +1,12 @@
 
 
     function MudaCorAutomatico() {
-        
+        let vermelho = document.getElementById('semaforo-verm')
+        let amarelo = document.getElementById('semanforo-amar')
+        let verde = document.getElementById('semaforo-verde')
+
+        setInterval(function(){
+
+        }, 2000)
+
     }

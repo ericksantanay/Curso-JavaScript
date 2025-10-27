c13420898fabe00075857bc11d8ed33252721646
Erick Silva <donizttierick@gmail.com>
2025-10-27 14:52:39 -0300

    .

---
+++ pratica2/30/index.js
@@ -0,0 +1,11 @@
+
+
+   
+
+   setInterval(function(){
+     if (body == 'white') {
+        document.body.style.backgroundColor = 'blue'
+     }else if (body == blue) {
+        document.body.style.backgroundColor = 'red'
+     }
+   }, 1000)

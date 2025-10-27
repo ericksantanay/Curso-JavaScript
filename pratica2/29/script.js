0aca6d8e0d368a791a0f75456faee13da532a7a1
Erick Silva <donizttierick@gmail.com>
2025-10-27 15:08:13 -0300

    .

--- pratica2/29/script.js
+++ pratica2/29/script.js
@@ -1,29 +1,5 @@
-
     let verm = document.getElementById('semaforo-verm')
 
     let a = document.getElementById('semanforo-amar')
 
     let ver = document.getElementById('semaforo-verde')
-
-    let corInicial = 'black'
-    let vermelho = ["red"]
-    let amarelo = ["yellow"]
-    let verde = ["green"]
-
-    indice = 0
-    function MudaCorAutomatico() {
-        
-
-        setInterval(function(){
-            verm.style.backgroundColor = (vermelho[indice])
-            if (indice > vermelho) {
-                indice = corInicial
-            }
-          
-           a.style.backgroundColor = (amarelo[indice])
-
-           ver.style.backgroundColor = (verde[indice])
-            indice++
-        }, 1000)
-
-    }

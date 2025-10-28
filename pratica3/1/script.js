cc10f7cc50c2424ce098b01e29f92b85dafdf5d0
Erick Silva <donizttierick@gmail.com>
2025-10-28 15:01:11 -0300

    .

--- pratica3/1/script.js
+++ pratica3/1/script.js
@@ -3,23 +3,26 @@
     const tempo = document.getElementById('tempoLigado')
     let sensor = document.getElementById('sensor')
     const cores = ["yellow", "orange", "blue"]
+
     // Função automatico
     indice = 0
     function Automatico() {
        
         setInterval(function(){
-        
         lampada.style.backgroundColor = (cores[indice])
         indice++
+        ifEelse()
         }, 2000)
         
-        ifEelse()
+        
     }
 
     // Função, ajustes 
-
     function ifEelse() {
+        // Parte de mudar as cores e voltar para o inicio de novo
         if (indice >= cores.length) {
             indice = 0
         }
     }
+
+

a168d94be7364910e7acc6eefcdb3c49de9c731d
Erick Silva <donizttierick@gmail.com>
2025-10-28 14:59:09 -0300

    .

--- pratica3/1/script.js
+++ pratica3/1/script.js
@@ -1,2 +1,25 @@
 
     const lampada = document.getElementById('lampada')
+    const tempo = document.getElementById('tempoLigado')
+    let sensor = document.getElementById('sensor')
+    const cores = ["yellow", "orange", "blue"]
+    // Função automatico
+    indice = 0
+    function Automatico() {
+       
+        setInterval(function(){
+        
+        lampada.style.backgroundColor = (cores[indice])
+        indice++
+        }, 2000)
+        
+        ifEelse()
+    }
+
+    // Função, ajustes 
+
+    function ifEelse() {
+        if (indice >= cores.length) {
+            indice = 0
+        }
+    }

b56c08a4593480538d3c8bf07312f7ab0fa6b341
Erick Silva <donizttierick@gmail.com>
2025-10-29 17:10:06 -0300

    .

--- pratica3/2/index.js
+++ pratica3/2/index.js
@@ -1,18 +1,28 @@
     const  t = document.getElementById('temperatura')
     let estado = document.getElementById('ambiente')
 
+
+    // Função com ajustes 
+    function ajustes() {
+        // Parte se o indice for menor que 0 
+        if (indice < 0  ) {
+            indice = 0
+        }
+    }
+
     // Aumentar Temperatura 
     indice = 0 
     function aumentarTemperatura() {
         indice++
         t.innerHTML = (` Temperatura ${indice}°C`)
-        
+       
     }
 
 
     // Diminuir Temperatura
     function diminuirTemperatura() {
-         indice--
+        indice--
+        ajustes()
         t.innerHTML = (`Temperatura ${indice}°C`)
-       
     }
+

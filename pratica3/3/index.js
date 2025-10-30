9c1c0a6203d0869f5329e8ee2660b0c3cf8eafcb
Erick Silva <donizttierick@gmail.com>
2025-10-30 20:09:27 -0300

    .

---
+++ pratica3/3/index.js
@@ -0,0 +1,20 @@
+
+    let r = document.getElementById('resultado')
+
+    // Parte de ajustes 
+    function ajustes() {
+
+        if (indice < 0 ) {
+            indice = 0
+        }
+    }
+
+    // Função de carregar 
+    indice = 0 
+    function Carregar() {
+        indice++
+        r.innerText = (`🔋 Bateria: ${indice}%`)
+        ajustes()
+    }
+
+    function

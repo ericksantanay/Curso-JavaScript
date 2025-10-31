db24ab57a48be1303102e3638dbd247292b60de6
Erick Silva <donizttierick@gmail.com>
2025-10-31 18:54:20 -0300

    .

---
+++ pratica3/4/script.js
@@ -0,0 +1,11 @@
+
+    let r = document.getElementById('humidade')
+    
+
+    // Função de regar 
+    indice = 0
+    function regar() {
+        setInterval(function(){
+
+        }, 1000)
+    }

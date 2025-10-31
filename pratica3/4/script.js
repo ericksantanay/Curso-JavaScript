5b3986d2cafce441ced10c6912f7fa129c8cb9f5
Erick Silva <donizttierick@gmail.com>
2025-10-31 19:06:28 -0300

    Pronto

--- pratica3/4/script.js
+++ pratica3/4/script.js
@@ -1,12 +1,39 @@
 
     let r = document.getElementById('humidade')
-    
+
+    // Essa é uma função que ajusta ex: se for menor que 0 
+    function ajustes() {
+        // Se for menor que 0
+        if (indice < 0 ) {
+            indice = 0
+        }
+
+        //Essa é se o indice for maior que 100
+        if (indice >= 100) {
+            indice = 100
+        }
+
+    }
 
     // Função de regar 
     indice = 50
     function regar() {
-        setInterval(function(){
+        tempo = setInterval(function(){
             indice++
+            ajustes()
             r.innerText = (`Humidade: ${indice}%`)
         }, 1000)
+    }
+
+
+    function secar() {
+        tempo = setInterval(function(){
+            indice--
+            ajustes()
+            r.innerText = (`Humidade: ${indice}%`)
+        }, 1000)
+    }
+
+    function parar() {
+        clearInterval(tempo)
     }

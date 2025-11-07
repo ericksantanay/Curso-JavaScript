f3646ca00d4408e8f83492df9b00644e74929fec
Erick Silva <donizttierick@gmail.com>
2025-11-07 18:08:38 -0300

    Pronto

--- pratica3/10/script.js
+++ pratica3/10/script.js
@@ -1,22 +1,32 @@
 
     
+    
+
 
     function Resultado() {
         // CHAMANDO OS INPUTS 
+        let sim = document.getElementById('isim')
+        let nao = document.getElementById('inao')
         let r = document.querySelector('.resposta')
-        let sim = document.getElementById('isim').value
-        let nao = document.getElementById('inao').value
         let sintoma = document.getElementById('itext').value
-
         
         if (sintoma == '') {
             alert('PREENCHA O CAMPO ABAIXO')
+            return
         }
 
-        if (Resultado == sim){
-           r.innerHTML = (`Voce ${sim} esta bem e o seu sintoma é ${sintoma}, voce precisa tomar dipirona`)
-        }else if (nao == nao){
-            r.innerHTML = (`Voce ${nao} esta bem e o seu sintoma é ${sintoma}, voce precisa tomar dipirona`)
+        if (sim.checked){
+            document.body.style.color = 'white'
+           r.innerHTML = (`Voce esta bem e o seu sintoma é ${sintoma}, voce vai <strong>morrer</strong>`)
+           
+        }else if (nao.checked){
+            document.body.style.color = 'white'
+            r.innerHTML = (`Voce não esta bem e o seu sintoma é ${sintoma}, toma dipirona que cura!`)
+            
         }
+
+        
 
     }
+
+    sintoma = ''

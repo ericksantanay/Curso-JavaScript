fdbd58770d0e6ad125a980ff14a0196c708b6294
Erick Silva <donizttierick@gmail.com>
2025-11-07 12:25:08 -0300

    .

--- pratica3/7/index.js
+++ pratica3/7/index.js
@@ -2,3 +2,12 @@
     let data = new Date()
     let h = document.getElementById('hora')
     let m = document.getElementById('minuto')
+    let s = document.getElementById('segundos')
+
+    setInterval(function(){
+        //HORA
+        let hora = data.getHours()
+        h.innerText = (hora)
+
+
+    }, 1000)

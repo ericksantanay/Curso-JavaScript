90c2c2b37a411b18c07584535470df153ceb269d
Erick Silva <donizttierick@gmail.com>
2025-11-08 14:33:49 -0300

    .

--- pratica3/11/script.js
+++ pratica3/11/script.js
@@ -1,6 +1,13 @@
 
+    let icon = document.getElementById('burguer')
+
+    function Click() {
+        
     if (menu.style.display == 'block') {
         menu.style.display = 'none'
+        icon.innerText = ('menu')
     }else {
         menu.style.display = 'block'
+        icon.innerText = ('close')
+    }
     }

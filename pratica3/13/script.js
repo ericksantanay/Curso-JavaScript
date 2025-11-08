b55cfd69a92bb12bf5c58cfd07c049d20c044e7b
Erick Silva <donizttierick@gmail.com>
2025-11-08 16:41:26 -0300

    .

--- pratica3/13/script.js
+++ pratica3/13/script.js
@@ -3,24 +3,25 @@
     let r = document.getElementById('respostaIdade')
     let res = document.getElementById('res')
    
-    
+    // Função Calcular
     function Calcular() {
     let idade = document.getElementById('itext').value
     let data = new Date()
     let anoAtual = data.getFullYear()
     let conta = anoAtual - idade
 
+    // Max de caracterias que o usuario pode digitar
     let maxCaracter = 4
 
+    // Se o input == a (nada) vai aparecer para o usuario preencher
     if (idade == '') {
         alert('Preencha o campo com a sua data de nascimento.')
     }else if (idade >  maxCaracter) {
-        alert('Coloque Números validos')
         idade.value = ''
+        alert('Coloque Números validos')
     }
 
-
-
+    
     if (conta < 18) {
         res.innerText = (`Voce é menor de idade!!`)
     }else {

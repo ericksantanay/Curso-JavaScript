4a90642b5b26405460ccaf69e3bab67b477d2b72
Erick Silva <donizttierick@gmail.com>
2025-11-08 15:29:27 -0300

    .

---
+++ pratica3/13/script.js
@@ -0,0 +1,6 @@
+
+//Puxando o input, o paragrafo que vai sair a resposta e colocando o new Date() e getFulYeaer()
+    let idade = document.getElementById('itext').value
+    let r = document.querySelector('.resposta')
+
+    //

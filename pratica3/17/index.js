2c75b605aa6dd8c21d08324164f52e584ed24a98
Erick Silva <donizttierick@gmail.com>
2025-11-10 14:40:49 -0300

    Finalizado

--- pratica3/17/index.js
+++ pratica3/17/index.js
@@ -1,3 +1,22 @@
 
+    let foto = document.getElementById('foto')
+
     // ARRAY COM AS FOTOS
-    let fotos = ["foto1.jpg", "foto2"]
+    let fotos = ["foto1.jpg", "foto2.png"]
+
+    indice = 0
+    function MudarImagem() {
+
+        
+       
+    }
+
+     setInterval(function(){
+            indice++
+
+        if (indice >= fotos.length) {
+            indice = 0
+        }
+
+        foto.src = (fotos[indice])
+        }, 5000)

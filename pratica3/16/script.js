7c59a467d2b0027cc8060f809849b344208689e1
Erick Silva <donizttierick@gmail.com>
2025-11-10 14:16:31 -0300

    pronto

--- pratica3/16/script.js
+++ pratica3/16/script.js
@@ -1,7 +1,28 @@
 
 
     let foto = document.getElementById('foto')
-    let fotos = ["foto1", "foto2"]
+    let fotos = ["foto1.jpg", "foto2.png"]
 
     indice = 0
-    
+    function proxima() {
+        indice++
+
+        if (indice > 1) {
+            indice = 0
+        }
+
+        foto.src = fotos[indice]
+         
+        
+    }
+
+
+    function anterior() {
+        indice--
+
+        if (indice < 0) {
+            indice = 1
+        }
+
+        foto.src = fotos[indice]
+    }

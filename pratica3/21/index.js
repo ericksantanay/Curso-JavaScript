ea2ed60424a1a0eef19355bb25020d48fd8ef328
Erick Silva <donizttierick@gmail.com>
2025-11-11 14:52:04 -0300

    .

---
+++ pratica3/21/index.js
@@ -0,0 +1,13 @@
+
+    let dados = []
+
+    function Adicionar() {
+
+    //Puxando os inputs
+    let Nome = document.getElementById('inome').value
+    let Preco = Number(document.getElementById('ipreco').value)
+
+
+    // Objeto 
+    let 
+}

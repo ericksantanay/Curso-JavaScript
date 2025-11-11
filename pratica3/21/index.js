965797987955f49e5c5d495855a73039461938c9
Erick Silva <donizttierick@gmail.com>
2025-11-11 15:05:32 -0300

    .

--- pratica3/21/index.js
+++ pratica3/21/index.js
@@ -1,5 +1,12 @@
-
+    // Array
     let dados = []
+
+    //Saida dos produtos e os preços
+    let r = document.getElementById('res')
+
+    // Total do produto
+    let total = document.getElementById('total')
+
 
     function Adicionar() {
 
@@ -9,5 +16,16 @@
 
 
     // Objeto 
-    let 
+    let compra = {
+        nome: Nome,
+        preço: Preco
+    }
+
+    // Adicionando o Push
+    dados.push(compra)
+
+    // Primeira saida 
+        r.innerText = ` ${nome} -- R$${preço}` 
+
+
 }

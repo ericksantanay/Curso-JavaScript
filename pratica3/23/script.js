c98e00da519b9edcc733a4ba1e6a132d747c4c88
Erick Silva <donizttierick@gmail.com>
2025-11-12 16:17:43 -0300

    .

--- pratica3/23/script.js
+++ pratica3/23/script.js
@@ -1,11 +1,29 @@
+    // Chamando o resul, aonde vai sair o codigo!
+    let r = document.getElementById('resul')
+  
+    // Função de adicionar 
+    function Adicionar() {
+    // Chamando o input 
+    let ipTarefa = document.getElementById('itarefa').value
+
+
+    // Objeto 
+    let objto = {
+        tarefa: ipTarefa
+    }
+
+    // Push
+    armazemTarefas.push(objto)
+
+    // Laço de repetição !!!
+    armazemTarefas.forEach(item => {
+        
+    });
+    }
+
+      
     // Array
     let armazemTarefas = []
 
     // Indice 
     indice = 0 
-
-    // Função de adicionar 
-    function Adicionar() {
-    // Chamando o input 
-    let ipTarefa = document.getElementById('itarefa').value
-    }

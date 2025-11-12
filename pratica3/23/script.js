447a07262915eb8fbe0eea5e655509142d697dbd
Erick Silva <donizttierick@gmail.com>
2025-11-12 16:14:22 -0300

    .

---
+++ pratica3/23/script.js
@@ -0,0 +1,11 @@
+    // Array
+    let armazemTarefas = []
+
+    // Indice 
+    indice = 0 
+
+    // Função de adicionar 
+    function Adicionar() {
+    // Chamando o input 
+    let ipTarefa = document.getElementById('itarefa').value
+    }

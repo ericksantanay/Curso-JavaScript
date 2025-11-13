43ff734a31ce3351ff9cb583dcd7850d455bc6d6
Erick Silva <donizttierick@gmail.com>
2025-11-13 16:37:40 -0300

    ajustando

--- pratica3/26/script.js
+++ pratica3/26/script.js
@@ -3,28 +3,28 @@ let t = document.getElementById('total')
 
 let armazem = []
 
-
 // Função de click
     function Adicionar() {
         //Input produto
-        let produto = document.getElementById('iproduto').value
+        let Produto = document.getElementById('iproduto').value
         // Input Preço
-        let preco = Number(document.getElementById('ipreco').value)
+        let Preco = Number(document.getElementById('ipreco').value)
 
-        // Objeto, serve como um pacotinho para deixar organizado no armazem 
-        let pacoteDeDados = {
-            Produto: produto,
-            Preço: preco
-        }
 
         // Eu coloco as ' ' para nao repetir o produto
-         
+        r.innerHTML = '' 
 
-        // Push serve para guardar o objeto dentro do array
-        armazem.push(pacoteDeDados)
+        // Objeto, serve como um pacotinho para deixar organizado no armazem 
+        let pacotinho = {produto: Produto, preço: Preco}
+
+         // Push serve para guardar o objeto dentro do array
+        armazem.push(pacotinho)
+
+        
 
         // Laço de repetição
-        array.forEach(element => {
-            
+        armazem.forEach(item => {
+            r.innerHTML += `<p>${item.produto}  ${item.preço}</p>`
+            t.innerHTML = (item.preço)
         });
     }

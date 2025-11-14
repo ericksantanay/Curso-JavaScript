d8a44f1aba22fb360c2c61a3f966fc8cc9272ce1
Erick Silva <donizttierick@gmail.com>
2025-11-14 18:37:33 -0300

    ...

--- pratica3/28/script.js
+++ pratica3/28/script.js
@@ -10,5 +10,23 @@ function adicionar() {
     let nome = document.getElementById('inome').value
     // Input Telefone
     let telefone = Number(document.getElementById('itelefone').value)
-    
+
+
+    // Para não repetir as listas
+    nome.value = ''
+
+    // Objeto, aqui é tipo os itens dentro de um pacotinho e o array é o armario
+    let dados = {
+        Nome: nome,
+        Telefone: telefone
+    }
+
+    // Metodo Push, serve para eu pegar o bjeto e por no array a minha "lista"
+    bancoDeDados.push(dados)
+
+    bancoDeDados.forEach(item => {
+    let nome = 
+        r.innerHTML += `<div><p>${item.Nome} <br> ${item.Telefone}  <span></span></p></div>`
+    });
+
 }

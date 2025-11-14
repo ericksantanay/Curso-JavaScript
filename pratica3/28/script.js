87944fe9b081c3a1a53d11f9a9e5a1175a797e94
Erick Silva <donizttierick@gmail.com>
2025-11-14 18:24:15 -0300

    Iniciado

---
+++ pratica3/28/script.js
@@ -0,0 +1,14 @@
+// Aonde vai aparecer o conteudo
+const r = document.getElementById('res')
+
+// Aonde eu vou guardar a lista 
+let bancoDeDados = []
+
+// Função de adicionar 
+function adicionar() {
+    // Input nome
+    let nome = document.getElementById('inome').value
+    // Input Telefone
+    let telefone = Number(document.getElementById('itelefone').value)
+    
+}

20e255bda14d80bb188086277a670b4ba4aeec9e
Erick Silva <donizttierick@gmail.com>
2025-11-15 17:07:06 -0300

    Ajustes

--- pratica3/29/script.js
+++ pratica3/29/script.js
@@ -1,6 +1,6 @@
 // O que o usuario vai ver 
 let result = document.getElementById('mensagem')
-
+let me = document.getElementById('media')
 // Array aonde vou Armazenar as listas 
 let bancoDeDados = []
 
@@ -9,27 +9,33 @@ let bancoDeDados = []
 // Função de cadastro!
 function cadastrar() {
     // Input Nome do aluno
-    let nome = document.getElementById('nome').value
+    let Nome = document.getElementById('nome').value
     //Input Nota 1 
-    let n1 = document.getElementById('nota1').value
+    let n1 = Number(document.getElementById('nota1').value)
     //Input Nota 2 
-    let n2 = document.getElementById('nota2').value
+    let n2 = Number(document.getElementById('nota2').value)
     //Input Nota 3 
-    let n3 = document.getElementById('nota3').value
+    let n3 = Number(document.getElementById('nota3').value)
 
     // Condições se o usuario nao digitar nada 
-    if (nome === '' || n1 === '' || n2 === '' || n3 === '') {
+    if (Nome === '') {
         alert('Preencha os campos abaixo')
         return
     }
 
     // Divisor da nota
-    //let divisor = 3
-
-    //let conta = n1 + n2 + n3 / divisor // Ajustar depois
-
-
-    result.innerText = (conta)
+    let totalProvas = 3
+    // Conta
+    let conta = (n1 +  n2 + n3) / totalProvas; // Ajustar depois
+    
+    // Aprovado ou reprovado
+    if (conta >= 5) {
+        me.innerHTML = ('aprovado')
+    }else {
+        me.innerHTML = ('reprovado')
+    }
+     
+     
     // Guardar os dados no navegador 
     localStorage.setItem('banco', JSON.stringify(bancoDeDados))
 
@@ -38,10 +44,10 @@ function cadastrar() {
 
     // Objeto com os dados
     let dados = {
-        Nome: nome,
-        Nota1: n1,
-        Nota2: n2,
-        Nota3: n3
+        nome: Nome,
+        nota1: n1,
+        nota2: n2,
+        nota3: n3
     }
 
     // Agora eu tenho  que puchar o dados no array "Lista"
@@ -50,7 +56,15 @@ function cadastrar() {
 
     // Laço de repetição que vai percorrer o array inteiro 
     bancoDeDados.forEach(item,(indice) => {
-        result.innerHTML =+ ``
+        result.innerHTML =+ `
+                <div id="containerTxt">
+                <p>Nome do aluno: ${item.nome}</p>
+                <p>Notas: Nota1:${item.nota1}, Nota2:${item.nota2}, Nota3:${item.nota3}</p>
+                <p>Media do aluno: ${conta.toFixed(2)}</p>
+                <p id="media">Aprovado/Reprovado:</p>
+                <span class="material-symbols-outlined" onclick="deletar(${indice})">delete</span>
+                </div>
+                `
     });
 
 
@@ -64,9 +78,29 @@ function cadastrar() {
     document.getElementById('nota1').value = ''
     document.getElementById('nota2').value = ''
     document.getElementById('nota3').value = ''
+    
 }
 
+// Função de apagar
+function deletar(indice) {
+    // Para não ter o problema de repetir os dados
+    result.innerHTML = ''
+    bancoDeDados.splice(indice, 1)
 
+
+    bancoDeDados.forEach(item, (indice) => {
+        result.innerHTML =+ `
+                <div id="containerTxt">
+                <p>Nome do aluno: ${item.nome}</p>
+                <p>Notas: Nota1:${item.nota1}, Nota2:${item.nota2}, Nota3:${item.nota3}</p>
+                <p>Media do aluno: ${conta.toFixed(2)}</p>
+                <p id="media">Aprovado/Reprovado:</p>
+                <span class="material-symbols-outlined" onclick="deletar(${indice})">delete</span>
+                </div>
+                `
+    });
+
+}
 
 
 

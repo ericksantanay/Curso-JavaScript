48234114f26c0670bc27addace8faa218b374a74
Erick Silva <donizttierick@gmail.com>
2025-11-15 15:39:49 -0300

    .

--- pratica3/29/script.js
+++ pratica3/29/script.js
@@ -1,21 +1,41 @@
 // O que o usuario vai ver 
 let result = document.getElementById('mensagem')
 
+// Array aonde vou Armazenar as listas 
+let localStorage.bancoDeDados = []
+
 // Função de cadastro!
 function cadastrar() {
     // Input Nome do aluno
     let nome = document.getElementById('nome').value
     //Input Nota 1 
-    let nota1 = Number(document.getElementById('nota1').value)
+    let n1 = document.getElementById('nota1').value
     //Input Nota 2 
-    let nota2 = Number(document.getElementById('nota2').value)
+    let n2 = document.getElementById('nota2').value
     //Input Nota 3 
-    let nota3 = Number(document.getElementById('nota3').value)
+    let n3 = document.getElementById('nota3').value
 
     // Condições se o usuario nao digitar nada 
-    if (nome === '' || nota1 === '' || nota2 === '' || nota3 === '') {
+    if (nome === '' || n1 === '' || n2 === '' || n3 === '') {
         alert('Preencha os campos abaixo')
+        return
     }
+
+    // Para não ter o problema de repetir os dados
+    result.innerHTML = ''
+
+    // Objeto com os dados
+    let dados = {
+        Nome: nome,
+        Nota1: n1,
+        Nota2: n2,
+        Nota3: n3
+    }
+
+    // Agora eu tenho  que puchar o dados no array "Lista"
+    bancoDeDados.push(dados)
+
+
 
 
 

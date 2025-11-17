931da31a672537b6ecc3d61780dd5136488931df
Erick Silva <donizttierick@gmail.com>
2025-11-17 13:35:01 -0300

    Ajustes

---
+++ pratica4/2/logicaSaque.js
@@ -0,0 +1,15 @@
+function sacarValor(sacar) {
+    let sacar = 350 - 100 - 50
+    let mensaguem = ''
+    if (sacar === 1,3) {
+        mensaguem = 'Valor inválido para saque'
+        return
+    }
+
+    return {
+        100: 3,
+        50: 1
+    }
+}
+
+console.log(sacarValor(350))

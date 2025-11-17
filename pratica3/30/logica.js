a14870f1f04634344e46ee80717c3df7ab2ee17a
Erick Silva <donizttierick@gmail.com>
2025-11-17 12:42:12 -0300

    Produzindo

---
+++ pratica3/30/logica.js
@@ -0,0 +1,14 @@
+let n1 = 5
+let n2 = 9
+let n3 = 7
+
+let media = (n1 + n2 + n3) / 3
+
+function situacao() {
+    if (media >= 7 ) {
+        console.log(aprovado)
+        return
+    }else if (media >= 5 && media < 7) {
+        
+    }
+}

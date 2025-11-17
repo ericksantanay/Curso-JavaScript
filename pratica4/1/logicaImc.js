2dd4f5ff249f3a85d82fa0a0002fe025d6a4b357
Erick Silva <donizttierick@gmail.com>
2025-11-17 13:12:53 -0300

    Finalizado

---
+++ pratica4/1/logicaImc.js
@@ -0,0 +1,22 @@
+function calcularIMC(peso,altura) {
+    let imc = peso / (altura * altura)
+    let situacao = ''
+    
+    if (imc < 18.5) {
+        situacao = 'Abaixo do peso'
+    }else if (imc >= 18.5 || imc < 24.9) {
+        situacao = 'Peso normal'
+    }else if (imc >= 25 && imc < 29.9) {
+        situacao = 'Sobrepeso'
+    }else {
+        situacao = 'Sobrepeso'
+    }
+
+
+    return {
+        imc: imc,
+        situacao: situacao
+    }
+}
+
+console.log(calcularIMC(10, 6, 9))

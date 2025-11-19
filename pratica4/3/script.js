d5bb62cc0a72ab348e99b26a47beeb62ba6f96e6
Erick Silva <donizttierick@gmail.com>
2025-11-19 17:10:47 -0300

    .

---
+++ pratica4/3/script.js
@@ -0,0 +1,11 @@
+//Puxando o menu hamburguer para adicionar um X
+
+// Parte do Menu
+function ClickMenu() {
+    if (menu.style.display == 'block') {
+        menu.style.display = 'none'
+    }else {
+        menu.style.display = 'block'
+    }
+}
+

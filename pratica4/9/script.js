21e4aefc582212dda3138c6f960010223c1ab92f
Erick Silva <donizttierick@gmail.com>
2025-11-30 18:41:44 -0300

    Finalizado

--- pratica4/9/script.js
+++ pratica4/9/script.js
@@ -1,12 +1,15 @@
 // Saida 
 const r = document.getElementById('res')
 
-let nomes = ["Erick", "Leandro", "Mateus", "Breno", "Bruno", "Bernardo", "Vinicius", "Layssa", "Larissa", "Lays", "Paulo"]
+let nome = ["Erick", "Leandro", "Mateus", "Breno", "Bruno", "Bernardo", "Vinicius", "Layssa", "Larissa", "Lays", "Paulo"]
 
 indice = 0
 setInterval(function(){
-
-    r.innerHTML = (indice[nomes])
+    r.innerHTML = (nome[indice])
     indice++
 
+    if (indice === nome.length) {
+        indice = 0 
+    }
 }, 1000)
+

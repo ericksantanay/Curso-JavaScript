3db1acd0609eb03a31c5dff32568bbaa1af30372
Erick Silva <donizttierick@gmail.com>
2025-11-30 18:31:52 -0300

    .

--- pratica4/8/script.js
+++ pratica4/8/script.js
@@ -4,6 +4,9 @@ let r = document.getElementById('res')
 
 const armazem = ["<div>oi</div>", "<div>P</div>", "<div>JHHH</div>", "<div>DDDDD</div>", "<div>DiDDDDDEEEEE</div>"]
 
+indice = 0 
 setInterval(function(){
-    
+    indice++
+    r.innerHTML = (armazem[indice])
+
 }, 1000)

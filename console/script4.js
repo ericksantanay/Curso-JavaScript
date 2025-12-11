0304269bc0e981f18218e4200df0da7b0d300105
Erick Silva <donizttierick@gmail.com>
2025-12-11 07:25:15 -0300

    Finalizado

---
+++ console/script4.js
@@ -0,0 +1,12 @@
+// Maior número
+let num1 = 50
+let num2 = 100
+
+console.log(num1,num2)
+console.log('Qual é maior ? Num1 que é 50 ou num2 que é 100')
+
+if (num1 > num2) {
+    console.log('Num1 <')
+}else {
+    console.log('> Num2')
+}

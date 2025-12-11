87f9b8e2058d20f25bf17aa34b5c4b69056dcb7a
Erick Silva <donizttierick@gmail.com>
2025-12-11 07:43:28 -0300

    Finalizado

---
+++ console/script5.js
@@ -0,0 +1,15 @@
+// Menor número entre tres
+let num1 = 20
+let num2 = 522
+let num3 = 700
+
+console.log('Quam desses números é maior?')
+console.log('Num1 =20, Num2 =522 ou Num3 =70')
+
+if (num1 > num2 && num3) {
+    console.log('Num1 é maior')
+}else if (num2 > num3 && num1) {
+    console.log('Num2 é maior')
+}else {
+    console.log('Num3 é Maior')
+}

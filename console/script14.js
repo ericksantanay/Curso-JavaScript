366799c9bdfd34869a2d69024a6b4bb0bc03d0e4
Erick Silva <donizttierick@gmail.com>
2025-12-12 11:21:24 -0300

    Finalizado

--- console/script14.js
+++ console/script14.js
@@ -1,1 +1,15 @@
-// Calcular Desconto
+//Criar função que retorna o maior de dois números
+
+function Maior(num1,num2) {
+    num1 = 5 
+    num2 = 6
+     
+    if (num1 > num2) {
+        console.log('Num1')
+    }else {
+        console.log('Num2')
+    }
+}
+
+Maior()
+

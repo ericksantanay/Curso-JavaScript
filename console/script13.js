e7135cd1627b61cfa1c09d79ad675e8d07e08af9
Erick Silva <donizttierick@gmail.com>
2025-12-12 10:58:28 -0300

    Inicio

---
+++ console/script13.js
@@ -0,0 +1,12 @@
+let nota = 4
+
+
+if (nota < 5) {
+    console.log('Ruim')
+}else if (nota >= 5 && nota <= 6 ) {
+    console.log('Media')
+}else if (nota >= 7 && nota <= 8) {
+    console.log('Boa')
+}else if (nota > 8 ) {
+    console.log('Excelente')
+}

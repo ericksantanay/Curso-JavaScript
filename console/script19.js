41f6c1df87f792d37b2717d56e67c5b5dd585658
Erick Silva <donizttierick@gmail.com>
2025-12-13 10:33:11 -0300

    .

---
+++ console/script19.js
@@ -0,0 +1,13 @@
+// Sitema de pontuação
+
+let pontuacao = 500
+
+if (pontuacao > 500) {
+    console.log('Ouro')
+}else if (pontuacao >= 100 && pontuacao < 500) {
+    console.log('Prata')
+}else if (pontuacao == 100){
+    console.log('Bronze')
+}else if (pontuacao < 100) {
+    console.log('Iniciante')
+}

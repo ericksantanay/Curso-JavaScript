f504b5ef0d08c4e7306c5f7545d5c850018e2140
Erick Silva <donizttierick@gmail.com>
2025-12-13 10:37:19 -0300

    Finalizado

--- console/script19.js
+++ console/script19.js
@@ -1,13 +1,14 @@
 // Sitema de pontuação
 
-let pontuacao = 500
+let pontuacao = 60
 
-if (pontuacao > 500) {
-    console.log('Ouro')
-}else if (pontuacao >= 100 && pontuacao < 500) {
+if (pontuacao == 1000) {
+    console.log('Ouro!')
+}else if (pontuacao == 500) {
     console.log('Prata')
-}else if (pontuacao == 100){
+}else if (pontuacao == 100) {
     console.log('Bronze')
-}else if (pontuacao < 100) {
+}else if (pontuacao < 100 ) {
     console.log('Iniciante')
 }
+

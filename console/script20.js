3e8f2b98a5fe8d39b77668a48ee00ac63f42d765
Erick Silva <donizttierick@gmail.com>
2025-12-13 11:02:15 -0300

    Finalizado

---
+++ console/script20.js
@@ -0,0 +1,11 @@
+// Desafio final 
+
+let idade = 22
+let carteira = true
+let carro = true
+
+if (idade >= 18 && carteira && carro) {
+    console.log('Você pode dirigir sozinho')
+} else {
+    console.log('Você não pode dirigir')
+}

a42613e2c7e151f6b99fc820bea8a5ce3909c23e
Erick Silva <donizttierick@gmail.com>
2025-12-13 10:25:44 -0300

    Finalizado

---
+++ console/script18.js
@@ -0,0 +1,10 @@
+// Temperatura
+let temp = 45
+
+if (temp >= 30) {
+    console.log('Quente!')
+}else if (temp >= 20 && temp < 30) {
+    console.log('Agradavel!')
+}else {
+    console.log('Frio!')
+}

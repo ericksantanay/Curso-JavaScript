98fb8c272d286e917bbbf357f2a321a6c85b2032
Erick Silva <donizttierick@gmail.com>
2025-12-13 10:21:59 -0300

    Finalizado

---
+++ console/script17.js
@@ -0,0 +1,10 @@
+// Numero maior ou menor que 10
+let num = 30
+
+if (num < 10) {
+    console.log('Menor')
+}else if (num === 10) {
+    console.log('Igual')
+}else if (num > 10) {
+    console.log('Numero Maior')
+}

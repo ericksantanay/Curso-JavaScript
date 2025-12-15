074a6abfdee40a71c0cb298293d59c27fcd24b68
Erick Silva <donizttierick@gmail.com>
2025-12-15 10:56:12 -0300

    Finalizado

---
+++ console/script30.js
@@ -0,0 +1,16 @@
+/*Loja
+Dados: estoque, reservado
+Defina o status do produto.
+
+*/
+
+let estoque = 21
+let reservado = false
+
+if (reservado === true) {
+    console.log('O estoque está reservado')
+} else if (estoque >= 10) {
+    console.log('Estoque Disponível')
+} else {
+    console.log('Estoque em Falta')
+}

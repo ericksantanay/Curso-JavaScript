5b4f731336a69d2d90502fdbf2e265bbb88fcb97
Erick Silva <donizttierick@gmail.com>
2025-12-15 10:14:11 -0300

    Finalizado

---
+++ console/script27.js
@@ -0,0 +1,8 @@
+//Atendimento
+let atendimento = 'Prioridade'
+
+if (atendimento === 'Prioridade') {
+    console.log('Atendimento de Prioridade')
+}else {
+    console.log('Esse Atendimento não precisa de Prioridade')
+}

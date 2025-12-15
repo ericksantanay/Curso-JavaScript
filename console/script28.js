c1f7e5e301e7e225c02d5851bd5ae916ff7713d8
Erick Silva <donizttierick@gmail.com>
2025-12-15 10:19:10 -0300

    Finalzado

---
+++ console/script28.js
@@ -0,0 +1,11 @@
+// Clima
+
+let clima = 28
+
+if (clima >= 30) {
+    console.log('Clima esta muito Quente!')
+}else if (clima >= 20 && clima < 30) {
+    console.log('O clima esta bom!')
+}else {
+    console.log('O clima esta frio!')
+}

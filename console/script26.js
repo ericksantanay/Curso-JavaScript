626f6adc3184ae5dfe260a35262a428fb8c10896
Erick Silva <donizttierick@gmail.com>
2025-12-15 09:53:05 -0300

    Finalizado

---
+++ console/script26.js
@@ -0,0 +1,10 @@
+// Transporte
+
+let carteira = 'Sim'
+let veiculo = 'Sim'
+
+if (carteira === 'Sim' && veiculo === 'Sim') {
+    console.log('Pode Dirigir!')
+}else {
+    console.log('Não Pode Dirigir!')
+}

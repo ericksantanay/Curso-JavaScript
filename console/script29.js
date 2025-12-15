d076e8ee3355a790df6608202767e9d0ca2ccc13
Erick Silva <donizttierick@gmail.com>
2025-12-15 10:40:27 -0300

    Finalizado

---
+++ console/script29.js
@@ -0,0 +1,15 @@
+/* Acesso completo
+Dados: usuarioAtivo, senhaCorreta, bloqueado
+Defina se o acesso é permitido, negado ou bloqueado.
+*/
+
+let usuario = 'Ativo'
+let senha = 'Errado'
+
+if (usuario === 'Ativo' && senha === 'Correta') {
+    console.log('Acesso Permitido')
+}else if (usuario === 'Inativo' && senha === 'Errado'){
+    console.log('Acesso Bloqueado ')
+}else {
+    console.log('Acesso Negado')
+}

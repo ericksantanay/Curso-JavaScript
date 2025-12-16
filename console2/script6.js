cac3fc6edbba5ec7185e0a01ee6ad5d585859322
Erick Silva <donizttierick@gmail.com>
2025-12-16 10:14:17 -0300

    Finalizado

--- console2/script6.js
+++ console2/script6.js
@@ -4,3 +4,14 @@ Jogo online
 Dados: internet, banido
 Defina se pode jogar.
 */
+
+let internet = true 
+let banido = true
+
+if (internet === true && banido === false) {
+    console.log('Pode jogar!')
+}else if (internet === true && banido === true) {
+    console.log('Voce esta Banido!')
+}else {
+    console.log('Voce Não Pode Jogar!')
+}

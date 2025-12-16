a237125f426116eefd2a083ac2a022b06c59cda7
Erick Silva <donizttierick@gmail.com>
2025-12-16 10:03:52 -0300

    Finalizado

--- console2/script5.js
+++ console2/script5.js
@@ -8,7 +8,7 @@ Defina se o aluguel é aprovado.
 let renda = 5000
 let fiador = true
 
-if (renda >= 1500 && filiado === true ) {
+if (renda >= 1500 && fiador === true ) {
     console.log('Aluguel Aprovado!')
 }else {
     console.log('Aluguel Reprovado!')

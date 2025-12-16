0261171adebbeb5efdf82d03a2f663f4b366855e
Erick Silva <donizttierick@gmail.com>
2025-12-16 09:44:22 -0300

    Finalizado

--- console2/script2.js
+++ console2/script2.js
@@ -6,4 +6,11 @@ Defina se o acesso é permitido.
 
 */
 
-let horario = 3
+let horario = 7
+let autorização = true 
+
+if (horario >= 1 && horario < 5 && autorização === true) {
+    console.log('Acesso Permitido!')
+}else {
+    console.log('Acesso Negado!')
+}

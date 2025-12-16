1872217d75008a203af376fcc5f12abda9ef4543
Erick Silva <donizttierick@gmail.com>
2025-12-16 10:03:18 -0300

    Finalizado

---
+++ console2/script5.js
@@ -0,0 +1,15 @@
+/* 
+Aluguel
+
+Dados: renda, fiador
+Defina se o aluguel é aprovado.
+*/
+
+let renda = 5000
+let fiador = true
+
+if (renda >= 1500 && filiado === true ) {
+    console.log('Aluguel Aprovado!')
+}else {
+    console.log('Aluguel Reprovado!')
+}

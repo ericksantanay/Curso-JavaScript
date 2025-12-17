52b17ef917cdbcfbaf83761ffecd2e40904fe9aa
Erick Silva <donizttierick@gmail.com>
2025-12-17 14:55:29 -0300

    Finalizado

---
+++ console2/script7.js
@@ -0,0 +1,14 @@
+/*
+Soma condicionada
+
+Dado um número, some de 1 até ele e mostre o resultado.
+
+*/
+
+let num = 40 
+let soma = 0
+
+for(let i = 0; i <= num; i++) {
+    soma += i
+}
+console.log(soma)

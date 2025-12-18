a225ca7b99d54b7b9b491ac1be45b6c30b6700b4
Erick Silva <donizttierick@gmail.com>
2025-12-18 10:53:47 -0300

    Finalizado

--- console2/script13.js
+++ console2/script13.js
@@ -4,10 +4,11 @@ Soma simples
 Dado um array de números, some todos eles.
 */
 
-let num = [5, 5, 5, 5]
-
+let num = [5, 5, 5, 5, 4]
 let conta = 0
 
 for(n of num) {
-    
+    conta = conta + n
 }
+
+console.log(conta)

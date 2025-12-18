feb6478c47d6ee5797559b2f5f861ebad8e132f1
Erick Silva <donizttierick@gmail.com>
2025-12-18 09:28:04 -0300

    .

--- console2/script10.js
+++ console2/script10.js
@@ -6,8 +6,14 @@ Dado um array de notas, calcule a média.
 
 let notas = [5, 7, 10, 8]
 
-let media = notas[0]
+let ar = notas[0]
+
+let conta = 0
 
 for(n of notas) {
-    
+
+conta = (n + ar) / 4 
+
 }
+
+console.log(conta)

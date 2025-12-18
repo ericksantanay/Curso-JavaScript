c8ebb5af7d3818e0fff359dcf9f17e2b97628acb
Erick Silva <donizttierick@gmail.com>
2025-12-18 10:15:40 -0300

    Finalizado

--- console2/script11.js
+++ console2/script11.js
@@ -6,14 +6,17 @@ Dado um array de notas, mostre quantos alunos foram reprovados.
 
 let notas = [5, 6, 3, 10, 2, 4]
 let media = 5 
+let c = 0
 
 for(n of notas) {
     if (n < media ) {
-        console.log(`Os alunos que não passaram tiraram ${n}`)
+        c++
     }
-   
-
 }
+
+console.log(c)
+
+
 
 
 

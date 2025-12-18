c38ddfee1951ee169313377c09b6b10eda161600
Erick Silva <donizttierick@gmail.com>
2025-12-18 08:58:04 -0300

    Finalizado

--- console2/script9.js
+++ console2/script9.js
@@ -4,12 +4,14 @@ Maior número
 Dado um array de números, mostre o maior.
 */
 
-let nuns = [34, 55, 2, 13, 567]
+let nums = [34, 55, 2, 13, 567, 1000]
 
+let maior = nums[0]
 
-
-for(n of nuns) {
-    nuns.filter(67)
+for (let n of nums) {
+    if (n > maior) {
+        maior = n
+    }
 }
 
-console.log(n)
+console.log(maior)

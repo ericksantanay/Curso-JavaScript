cb44b4246f5d42526f1e69a4085e2754a279fe9d
Erick Silva <donizttierick@gmail.com>
2025-12-18 10:47:45 -0300

    Finalizado

--- console2/script12.js
+++ console2/script12.js
@@ -2,18 +2,20 @@
 
 Dado um array e um número, diga se ele existe no array.
 */
-let escolha = 55
-
+let escolha = 5
 let num = [43, 55, 9, 10, 344, 244]
-indice = 0
 
-for(n of num) {
-    if (escolha === n) {
-        indice++
-        console.log(n)
-    }else {
-        console.log('Não Temos esse número')
+let achou = false
+
+for (let n of num) {
+    if (n === escolha) {
+        achou = true
     }
 }
 
+if (achou) {
+    console.log(`O número ${escolha} existe no array`)
+} else {
+    console.log(`O número ${escolha} não existe no array`)
+}
 

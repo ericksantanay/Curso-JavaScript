27c46ed6ff6c9517d6bf7abe1279d49157897fec
Erick Silva <donizttierick@gmail.com>
2025-12-19 12:35:58 -0300

    Finalizado

--- console2/script18.js
+++ console2/script18.js
@@ -3,3 +3,13 @@ Soma acumulada
 
 Dado um array, mostre a soma acumulada passo a passo.
 */
+
+let num = [1, 3, 5, 10, 20, 30]
+
+let soma = 0
+
+for(n of num) {
+    soma = soma + n
+
+    console.log(soma)
+}

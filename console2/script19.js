abcd744a6a805f8692c7a398acdfd8ace70044a9
Erick Silva <donizttierick@gmail.com>
2025-12-19 13:32:36 -0300

    Finalizado

--- console2/script19.js
+++ console2/script19.js
@@ -3,13 +3,23 @@ Segundo maior
 
 Dado um array de números, mostre o segundo maior valor.
 */
-// Segundo numero é o 8
 
 let num = [2, 4, 8, 10]
-let indice = 0
 
-for(n of num) {
-    
+let maior = -Infinity
+let segundoMaior = -Infinity
 
+for (let n of num) {
+    if (n > maior) {
+        segundoMaior = maior
+        maior = n
+    } else if (n < maior && n > segundoMaior) {
+        segundoMaior = n
+    }
 }
+
+console.log(segundoMaior)
+
+
+
 

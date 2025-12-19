e99cb24c28518b7f0c148690fc7d1782bdf4329e
Erick Silva <donizttierick@gmail.com>
2025-12-19 12:06:05 -0300

    Finalizado

--- console2/script16.js
+++ console2/script16.js
@@ -6,8 +6,13 @@ Dado um array de idades, diga quantas pessoas podem dirigir.
 
 let idades = [10, 30, 50, 22, 8, 18]
 
-indice = 0
+
+let indice = 0
 
 for(i of idades) {
-    
+    if (i >= 18) {
+        indice++
+    }
+        
 }
+console.log(indice)

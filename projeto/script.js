de815e25067d3c2a37932c30fe2bb68f4636e925
Erick Silva <donizttierick@gmail.com>
2026-01-05 15:38:19 -0300

    Iniciado

--- projeto/script.js
+++ projeto/script.js
@@ -1,8 +1,20 @@
-//######### ARRAY PARA ARMAZENAR OS ITENS #########
-let armazenarDados = []
+//######## MANIPILA O DOM #########
+let resultado1 = document.getElementById('resultado1')
+let resultado2 = document.getElementById('resultado2')
+let resultado3 = document.getElementById('resultado3')
+let resultado4 = document.getElementById('resultado4')
+
+//####### RESULTADO DAS TAREFAS#########
+let resultTarefas = document.querySelector('.resultadoListaTarefas')
 
 
-//######### FUNÇÃO DE ADICIONAR #########
-function Adicionar() {
-    let tarefas = document.getElementById('tarefa')
+
+//####### FUNÇÃO DE ADICIONAR #########
+function AdicionarTarefa() {
+    // INPUT DA TAREFA
+    let tarefa = document.getElementById('itarefa').value
+
+    // PEGANDO O VALUE DO SELECT
+    let opcoes = document.querySelector('.opcoesPrioridade')
 }
+
